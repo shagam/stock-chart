48fe474e8ef6198d7f48a49022679da5937cd2ac
Eli Shagam <eli.shagam@gmail.com>
2021-10-15 13:51:38 +0300

    Save temp handleCallBack

--- src/App.js
+++ src/App.js
@@ -8,18 +8,21 @@ import StockChart from './StockChart';
 import './App.css';
 import './react-tables.css';
 import StockTable from './Stock-table';
+import Overview from "./Overview";
 
 //<Stock StockSymbol="IBM" API_KEY = 'C542IZRPH683PFNZ' />
-
+ 
 /*
       <div>
         <StockList API_KEY = 'C542IZRPH683PFNZ' />
       </div>
       <div>
-         <Overview StockSymbol="IBM" API_KEY = 'C542IZRPH683PFNZ' />
+         <Overview StockSymbol={StockSymbol} API_KEY = 'C542IZRPH683PFNZ' />
       </div>
-
-
+      <div>
+        <Overview StockSymbol={StockSymbol}  API_KEY = {API_KEY}  callBack = {handleCallBack}/>  
+        <Overview StockSymbol={StockSymbol}  API_KEY = {API_KEY}  callBack = {handleCallBack}/> 
+      </div>
 */
 
 function App() {
@@ -29,17 +32,27 @@ function App() {
   //let url = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${StockSymbol}&apikey=${API_KEY}`
   //const date = new Date();
   //let date1 = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
-//             <td>{Moment(this.props.stat.dateFrom).format('yyy-MM-DD')}</td>
-  
+  //             <td>{Moment(this.props.stat.dateFrom).format('yyy-MM-DD')}</td>
+
+const handleCallBack = (childData) => {
+  //this.setState({msg: childData})
+  console.log (childData);
+  console.log (childData["Symbol"], childData["Exchange"], childData["Sector"], childData["EPS"],
+  childData["PERatio"], childData["PEGRatio"], childData["Beta"], childData["MarketCapitalization"]);
+}
+
   return (
     <div className="App-continer">
       <div>
+       
         <StockTable StockSymbol={StockSymbol} API_KEY = 'C542IZRPH683PFNZ'/>
       </div>
       <div>
          <StockChart StockSymbol={StockSymbol} API_KEY = {API_KEY} />
       </div>      
-
+      <div>
+ 
+      </div>
 
     </div>
 
