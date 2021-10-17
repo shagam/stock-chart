64af28343b795a66c18d52975249e6a3a88aaf48
Eli Shagam <eli.shagam@gmail.com>
2021-10-17 22:23:38 +0300

    Overview.js  some cleanup

--- src/Overview.js
+++ src/Overview.js
@@ -1,7 +1,6 @@
 //
 
 import React from 'react';
-//import GetURLInfo from "./GetURLInfo";
 
 
 // https://quant.stackexchange.com/questions/26162/where-can-i-get-a-list-of-all-yahoo-finance-stocks-symbols
@@ -11,8 +10,7 @@ class Overview extends React.Component {
         super(props);
 
         this.state = {
-            //stockChartXValues: [],
-            //stockChartYValues: []
+
         }
     }
 
@@ -21,13 +19,10 @@ class Overview extends React.Component {
     }
 
     fetchStock() {
-        //const pointerToThis = this;
-        //console.log (pointerToThis);
         const API_KEY = this.props["API_KEY"];
         const StockSymbol = this.props["StockSymbol"];
         const callBack = this.props["callBack"];
-        //const StockSymbol = localStorage.getItem('StockSymbol'); 
-        console.log (`${this.props}`);
+ 
 
 
         //let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_${periodCapital}_ADJUSTED&symbol=${StockSymbol}&outputsize=compact&apikey=${API_KEY}`;
@@ -47,7 +42,7 @@ class Overview extends React.Component {
                     //console.log(`${API_KEY}`);
                     //console.log(data);
                     const dataStr = JSON.stringify(data);
-                    localStorage.setItem('overview', `${dataStr}`);
+                    localStorage.setItem(`${StockSymbol}` + '_overview', `${dataStr}`);
                     callBack(data);
                 }
             )
