70c04ceb880f65ec83ad0fa18a83803e5e62a48d
Eli Shagam <eli.shagam@gmail.com>
2021-10-19 23:23:38 +0300

    Put back validety check

--- src/StockChart.js
+++ src/StockChart.js
@@ -65,8 +65,8 @@ class StockChart extends React.Component {
             .then(
                 (data) => {
                     const dataStr = JSON.stringify(data);
-                    //if ({StockSymbol})
-                        //localStorage.setItem (`${StockSymbol}` + '_chart', dataStr);
+                    if ({StockSymbol} != null && data != null && data['Symbol'] != null)
+                        localStorage.setItem (`${StockSymbol}` + '_chart', dataStr);
 
                     //let periodTag = 'Time Series (Daily)';
                     let periodTag = 'Weekly Adjusted Time Series';
