9d27dce08b1d6dbce40eebedb89c502c8d2cfcc1
Eli Shagam <eli.shagam@gmail.com>
2021-11-03 11:52:10 +0200

    Remove warn, some clean

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import './react-tables.css';
 import StockTable from './Stock-table';
 import StockChart from './StockChart';
+import Stock_chart from './Stock-chart';
 import AlphaVantage from './AlphaVantage';
 
 //<Stock StockSymbol="IBM" API_KEY = 'C542IZRPH683PFNZ' />
@@ -23,15 +24,12 @@ import AlphaVantage from './AlphaVantage';
 */
 
 function App() {
-  const ALPHA_WARNING = 'Alpha Vantage! Our standard API call frequency is 5 calls per minute and 500 calls per day';
-  
+ 
   //const StockSymbol = window.$StockSymbol;
   const StockSymbol = localStorage.getItem('chartSymbol');
   const API_KEY='C542IZRPH683PFNZ';  
-  //let url = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${StockSymbol}&apikey=${API_KEY}`
-  //const date = new Date();
-  //let date1 = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
-  //             <td>{Moment(this.props.stat.dateFrom).format('yyy-MM-DD')}</td>
+
+
 
 const handleCallBack = (childData) => {
   //this.setState({msg: childData})
@@ -43,10 +41,10 @@ const handleCallBack = (childData) => {
   return (
     <div className="App-continer">
       <div>       
-        <StockTable API_KEY = {API_KEY} WARN = {ALPHA_WARNING}/>
+        <StockTable API_KEY = {API_KEY} />
       </div>
       <div>
-         {/* <StockChart StockSymbol={StockSymbol} API_KEY = {API_KEY} callBack = {handleCallBack} WARN = {ALPHA_WARNING} /> */}
+         {/* <StockChart StockSymbol={StockSymbol} API_KEY = {API_KEY} callBack = {handleCallBack}  /> */}
       </div>      
       <div>
         {/* <AlphaVantage/> */}
