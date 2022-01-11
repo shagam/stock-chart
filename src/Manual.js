2c28edf1d5120c374c81b2ea974d73576c591dfa
Eli Shagam <eli.shagam@gmail.com>
2022-01-11 21:14:18 +0200

    Save temp work on Manual.pdf

--- src/Manual.js
+++ src/Manual.js
@@ -3,11 +3,14 @@ import React, {useState, useEffect} from 'react'
 import txt from './Manual.text'
 import './Manual.css'
 
+// import pdf from './Manual.pdf'
+// import {Document,Page} from 'react-pdf'
 
 const Manual = () => {
   const [ManualFlag, setManualFlag] = useState(false);
   const [manualText, setManualText] = useState("One\nTwo\nThree");
-
+  // const [manualPdf, setManualPdf] = useState("")
+  // const [numPages, setNumPages] = useState(null)
   
   const usageHelpChange = () => {setManualFlag (! ManualFlag)}
 
@@ -19,6 +22,14 @@ const Manual = () => {
       setManualText(text);
       //console.log (text)
     })
+
+    // fetch (pdf)
+    // .then (r => r.text())
+    // .then (pdf => {
+    //   setManualText(pdf);
+    //   //console.log (text)
+    // })
+
   }, [])
 
 
@@ -42,6 +53,14 @@ const Manual = () => {
             defaultValue={manualText}
           >
           </textarea>
+          {/* <div>
+            <Document file='C:/Manual.pdf'  onLoadSuccess={({ numPages }) =>setNumPages(numPages)} style={{width:'100%'}}>
+              {Array.apply(null, Array(numPages))
+              .map(page =>
+                <Page pageNumber={page} size='A4' />
+              )}
+            </Document>
+          </div> */}
         </div>
       }
     </div>
