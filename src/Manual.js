8027875d272e6c944d8afecd989a1ec089768be7
Eli Shagam <eli.shagam@gmail.com>
2022-01-12 22:23:30 +0200

    Attach Manual.pdf

--- src/Manual.js
+++ src/Manual.js
@@ -1,18 +1,29 @@
 
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useMemo, Component} from 'react'
 import txt from './Manual.text'
 import './Manual.css'
 
-// import pdf from './Manual.pdf'
+import filePDF from './Manual.pdf'
 // import {Document,Page} from 'react-pdf'
+
+// import Viewer, { Worker } from '@phuocng/react-pdf-viewer';
+// import '@phuocng/react-pdf-viewer/cjs/react-pdf-viewer.css';
 
 const Manual = () => {
   const [ManualFlag, setManualFlag] = useState(false);
   const [manualText, setManualText] = useState("One\nTwo\nThree");
   // const [manualPdf, setManualPdf] = useState("")
   // const [numPages, setNumPages] = useState(null)
-  
+  // const [pageNumber, setPageNumber] = useState(1)
+
+  //const pdfText_ = useMemo(() => pdf, []);
+
   const usageHelpChange = () => {setManualFlag (! ManualFlag)}
+
+  // const  onDocumentLoadSuccess = ({numPages}) => {
+  //   console.log ('loaded');
+  //   setNumPages(numPages);
+  // }
 
   // read helpFile into helpText
   useEffect (() => { 
@@ -23,17 +34,10 @@ const Manual = () => {
       //console.log (text)
     })
 
-    // fetch (pdf)
-    // .then (r => r.text())
-    // .then (pdf => {
-    //   setManualText(pdf);
-    //   //console.log (text)
-    // })
-
   }, [])
 
-
-
+  // onLoadError {console.error}
+try {
   return <div className='upload-expense'>
     <label>
           <input
@@ -44,27 +48,29 @@ const Manual = () => {
     <div className='txt'>
       {ManualFlag &&
         <div className='text'> 
-          <textarea
-            type='text'
-            name='manual'
-            cols='80'
-            rows='30'
-            readOnly
-            defaultValue={manualText}
-          >
-          </textarea>
-          {/* <div>
-            <Document file='file:///C:/Manual.pdf'  onLoadSuccess={({ numPages }) =>setNumPages(numPages)} style={{width:'100%'}}>
-              {Array.apply(null, Array(numPages))
-              .map(page =>
-                <Page pageNumber={page} size='A4' />
-              )}
+          {/* <textarea type='text' name='manual' cols='80' rows='30' readOnly
+           defaultValue={manualText}  >
+          </textarea> */}
+
+          
+          <div>       
+          <object data={filePDF} type="application/pdf" width="100%" /*height="100%"*/>  </object>
+     
+          {/* <object data="http://africau.edu/images/default/sample.pdf" type="application/pdf" width="200%" height="200%">   </object> */}
+
+            {/* <Document
+             file="./Manual.pdf"
+              // file='https://publications.mfo.de/bitstream/handle/mfo/3593/OWR_2017_28.pdf?sequence=1&isAllowed=y'
+               onLoadSuccess={onDocumentLoadSuccess}>
+                <Page pageNumber={pageNumber} />
             </Document>
-          </div> */}
+            <p> Page = {pageNumber} of {numPages} </p> */}
+          </div>
         </div>
       }
     </div>
   </div>;
+} catch (e) {console.log (e)}
 };
 
 export default Manual;
