06302b2589bab1f24266d486c5b5f9e8907335a6
Eli Shagam <eli.shagam@gmail.com>
2022-02-15 14:01:53 +0200

    Add props saveTable to <splits/>

--- src/table/BasicTable.js
+++ src/table/BasicTable.js
@@ -760,7 +760,7 @@ export const BasicTable = (props) => {
 
       <Stock_chart StockSymbol ={chartSymbol} stockChartXValues = {stockChartXValues}  stockChartYValues = {stockChartYValues}    splitsFlag = {splitsFlag} />
        
-      <Splits symbol ={chartSymbol} rows = {rows} API_KEY = 'BC9UV9YUBWM3KQGF' admin = {admin} />
+      <Splits symbol ={chartSymbol} rows = {rows} API_KEY = 'BC9UV9YUBWM3KQGF' admin = {admin} saveTable = {saveTable} />
 
       <GainValidate symbol ={chartSymbol} rows = {rows} stockChartXValues = {stockChartXValues}  stockChartYValues = {stockChartYValues} gain_validation_json={gain_validation_json} refreshCallBack = {props.refreshCallBack} />
       
