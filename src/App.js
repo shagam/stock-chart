a0ec3a8d2be871322e0d0231868a652cdff0a07c
Eli Shagam <eli.shagam@gmail.com>
2022-04-05 20:07:34 +0300

    signInWithFacebook

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 // import StockTable from './Stock-table';
 import {BasicTable} from './table/BasicTable' 
 import { auth } from './table/firebase-config'
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider, signInWithPopup } from 'firebase/auth'
 
 function App() {
   const [count, setCount] = useState (0);
@@ -12,25 +12,41 @@ function App() {
     setCount (count + childData);
   }
 
-  const provider = new GoogleAuthProvider()
+
   const signInWithGoogle = () => {
-     signInWithPopup(auth, provider)
+    const googleProvider = new GoogleAuthProvider()
+    signInWithPopup(auth, googleProvider)
      .then ((result) => {
-  
        const profilePic = result.user.photoURL;
        const phone = result.user.phoneNumber;
        console.log(result, result.user.email, result.user.name, profilePic, phone)
-  
      }).catch((error) => {
        alert (error.message)
      })
   };
 
 
+  const signInWithFacebook = () => {
+    const facebookProvider = new FacebookAuthProvider()
+    signInWithPopup(auth, facebookProvider)
+     .then ((result) => {
+       const profilePic = result.user.photoURL;
+       const phone = result.user.phoneNumber;
+      //  console.log(result, result.user.email, result.user.name, profilePic, phone)
+       console.log(result)
+     }).catch((error) => {
+       alert (error.message)
+     })
+  };
+//  Firebase: Error (auth/account-exists-with-different-credential).
+// ngrok http 3000
+
+
   return (
     <div className="App-continer">
       <div> 
         <button onClick={signInWithGoogle}> Sign In With Google</button>      
+        <button onClick={signInWithFacebook}> Sign In With Facebook</button>    
       </div>
       <div>
         <BasicTable refreshCallBack = {refreshCallBack} />
