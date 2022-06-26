0f47f8666a4ce8db5c31dd0a67e9dec4dae90611
Eli Shagam <eli.shagam@gmail.com>
2022-06-27 02:55:10 +0300

    Still fail through router

--- src/splits/StockSplitsGet.js
+++ src/splits/StockSplitsGet.js
@@ -6,10 +6,17 @@ import cors from 'cors'
 // http://84.228.164.65:5000/splits?stock=MSFT
 // https://www.stocksplithistory.com/?symbol=MSFT
 
+function getDate() {
+  const today = new Date();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  // var formattedDate = format(date, "yyyy-MMM-dd HH:mm");
+  return date + " " + time;    
+}
+
 export const StockSplitsGet = (sym, rows, saveTable, refreshCallBack) => {
 
       // const [splits, setSplits] = useState([])
-      var splitsArray = [];
 
       console.log (sym, rows.length)
       // function getSplits () {
@@ -17,27 +24,18 @@ export const StockSplitsGet = (sym, rows, saveTable, refreshCallBack) => {
       // var url = "https://www.stocksplithistory.com/?symbol=" + sym;
       //  url = 'www.google.com'
       var corsUrl = "http://84.228.164.85:5000/splits?stock=" + sym;
-      corsUrl = "http://192.168.1.4:5000/splits?stock=" + sym;
-      corsUrl = "http://localhost:5000/splits?stock=" + sym;
+      // corsUrl = "http://192.168.1.4:5000/splits?stock=" + sym;
+      // corsUrl = "http://localhost:5000/splits?stock=" + sym;
 
-      fetch(corsUrl, {mode:'cors'})
-      .then(
-            function(response) {
-                  const respStr = JSON.stringify (response);
-                  if (response.status !== 200)// (' status: 200, ok: true') !== -1)
-                  console.log(response);
-                  // return response;
-            }
-      )
-      .then(
-            function (data) {
-            const dataStr = JSON.stringify(data);
-            console.log (data);
-            // console.log (dataStr.substring(0,150));
-            }
-
-      )
-      return (splitsArray)
+      axios.get (corsUrl)
+      // getDate()
+      .then ((result) => {
+        console.log (getDate(), result.data, result.status, corsUrl)
+      })
+      .catch ((err) => {
+        console.log(err)
+      })
+    
         
 }
 
