bb48aa837c4e2b0104e09cf3d52d17a2b16f8dfd
Eli Shagam <eli.shagam@gmail.com>
2022-06-27 03:31:30 +0300

    Correct external router ip

--- src/splits/StockSplitsGet.js
+++ src/splits/StockSplitsGet.js
@@ -23,7 +23,7 @@ export const StockSplitsGet = (sym, rows, saveTable, refreshCallBack) => {
             // sym = 'TSLA'
       // var url = "https://www.stocksplithistory.com/?symbol=" + sym;
       //  url = 'www.google.com'
-      var corsUrl = "http://84.228.164.85:5000/splits?stock=" + sym;
+      var corsUrl = "http://84.228.164.65:5000/splits?stock=" + sym;
       // corsUrl = "http://192.168.1.4:5000/splits?stock=" + sym;
       // corsUrl = "http://localhost:5000/splits?stock=" + sym;
 
