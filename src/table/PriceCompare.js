78c173345a720ebb974e70978a26c6099c68d1a0
Eli Shagam <eli.shagam@gmail.com>
2022-06-29 12:09:27 +0300

    SAve some info of the start of PriceCompare

---
+++ src/table/PriceCompare.js
@@ -0,0 +1,19 @@
+
+// https://www.google.com/finance/quote/.IXIC:INDEXNASDAQ?window=6M
+
+// https://www.google.com/finance/quote/AMZN:NASDAQ?window=6M
+
+// https://www.google.com/finance/quote/AMZN:NASDAQ?window=1M
+
+// https://www.google.com/finance/quote/IBM:NYSE?window=5Y
+// https://www.google.com/finance/quote/TSLA:NASDAQ?window=5Y
+
+<title>AMZN $113.82 (▲0.53%) Amazon.com, Inc. | Google Finance</title>
+
+
+<span jsname="Fe7oBc" class="NydbP nZQ6l tnNmPe" data-disable-percent-toggle="true" data-multiplier-for-price-change="1" aria-label="Up by 136.10%"><div jsname="m6NnIb" class="zWwE1"><div class="JwB6zf" style="font-size: 16px;"><span class="V53LMb" aria-hidden="true"><svg focusable="false" width="16" height="16" viewBox="0 0 24 24" class=" NMm5M"><path d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z"></path></svg></span>136.10%</div></div></span>
+
+
+<span jsname="V67aGc" class="VfPpkd-jY41G-V67aGc">5Y</span>
+
+<span jsname="V67aGc" class="VfPpkd-jY41G-V67aGc">5Y</span>
