134e0fcf224a4c8b8d926fa0534472155370bc3c
Eli Shagam <eli.shagam@gmail.com>
2022-08-18 18:47:14 +0300

    InputNumber  Add pattern, submit cause refresh

--- src/table/InputNumber.js
+++ src/table/InputNumber.js
@@ -1,17 +1,13 @@
 import {useState} from 'react';
 
 const InputNumber = (props) => {
+  const LOG = false
   const [value, setValue] = useState(props.init);
 
-  const handleChange = event => {
-    const result = event.target.value//.replace(/\D/g, '');
-    setValue(result);
-    props.setNumber(result)
-    // console.log (result)
-  };
 
   function submit () {
     props.setNumber(value)
+    if (LOG)
     console.log(value);
   // console.log(typeof value);
   // console.log(Number(value));
@@ -23,9 +19,11 @@ const InputNumber = (props) => {
         <div>{props.title} &nbsp; &nbsp; </div>
         <input
           type="text"
+          pattern="[\-]?[0-9]+"
           placeholder={props.title}
           value={value}
-          onChange={handleChange}
+          onChange={(e)=>
+            setValue((v) => e.target.validity.valid ? e.target.value : v)}
         />
         <button type="submit"> submit </button> 
       </div>
