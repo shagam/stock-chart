b99243744f4e86117f200cb3d295f962f52aa3b1
eli shagam <eli.shagam@gmail.com>
2023-01-23 00:12:51 +0200

    modified:   src/table/ErrorList.js  add scroll to errors

--- src/table/ErrorList.js
+++ src/table/ErrorList.js
@@ -2,26 +2,35 @@ import React, {useState} from 'react'
 // import ComboBox from 'react-responsive-combo-box'
 // import 'react-responsive-combo-box/dist/index.css'
 import Dropdown from 'react-bootstrap/Dropdown';
+import { Container } from 'react-bootstrap'
 
 export function ErrorList (props) {
     const [visible, setVisible] = useState(false)
-    return (     
-        <div  className=' text-left mt-10'> 
-            <div style={{'color': 'red'}}>
+
+    // style={{display: 'flex'}}
+    return (  
+  
+        <div  >   
+            <div style={{color: 'red'}}>
                 <input 
                     type="checkbox" checked={visible}
                     onChange={() => {setVisible (!visible)}}
                 /> errors
             </div>
 
-           {/* <hr/>  */}
-             { visible && props.errorList.map((d) => (
-             <div key={d[1]}>
-                 {d[0]}  &nbsp; {d[1]}
-             </div>
-           ))
-           }
+            <Container  className='d-flex align-items-left justify-content-left' style={{maxHeight: "15vh"}}  > 
+            {/* <hr/>  */}
+            <div className="container" style ={{overflow: 'scroll', hight: '17%'}} >
+                { visible && props.errorList.map((d) => (
+                <div key={d[1]}>
+                    {d[0]}  &nbsp; {d[1]}
+                </div>
+                ))
+                }
+            </div>
+           </Container>
         </div>
+
     )
 
 }
