d31ade6d80feebbb07656f1aa4c0502a3ba1e783
eli shagam <eli.shagam@gmail.com>
2024-02-12 22:59:34 +0200

    Text change 	modified:   src/About.js

--- src/About.js
+++ src/About.js
@@ -19,22 +19,12 @@ return (
     {/* <h5  style={{color:'red'}}> for long term investors.  </h5> */}
 
 
-    <div style={{display:'flex'}} >
-        <h6 style={{color:'red'}}> AlphVatage.co </h6> <h6> &nbsp; Analysis based on Historical stock prices</h6>
-    </div>
-    
-    <div style={{display:'flex'}}>
-        <h6 style={{color:'red'}}> Free sharing </h6> <h6>&nbsp; Pease share with anyone interested </h6>
-    </div>
-
-    <hr/> 
-
-    <div style={{display:'flex'}} >
+      <div style={{display:'flex'}} >
         <h6 style={{color:'red'}}> Long term yearly growth (peak2Peak) </h6> <h6> &nbsp; - from the peak before the 2008 deep, to the peak before 2022 deep  . </h6>
     </div>
 
-    <h6>  &nbsp; &nbsp; Hi-Tech NASDAQ ETF (QQQ) has an average annual gain of 15%. </h6>
-    <h6>   &nbsp; &nbsp; Behemoth S&P DIA  have an average annual gain of about 10%. </h6>
+    <h6>  &nbsp; &nbsp; Hi-Tech NASDAQ ETF (QQQ) - average annual gain of 15%. </h6>
+    <h6>   &nbsp; &nbsp; S&P 500 - average annual gain of about 10%. </h6>
     
     <div style={{display:'flex'}} >
         <h6 style={{color:'red'}}>  Drop recovery analysis </h6> <h6> &nbsp; - calculate the drop level, and recovery period. </h6>
@@ -45,20 +35,12 @@ return (
         <h6>,   2022  QQQ  Drops 36%,  almost recovered within 2 years   . </h6>
     </div>
 
-    <div style={{display:'flex'}} >
+    {/* <div style={{display:'flex'}} >
         <h6 style={{color:'red'}}>volatility </h6> <h6> &nbsp; During deep periods, Hi-Tech drop 50% more than S&P. However Hi-Tech grows more during bear market.  </h6>
-    </div>
-
-    <div style={{display:'flex'}} >
-        <h6 style={{color:'red'}}> Target price </h6> <h6> &nbsp; - based an many analysts prediction. Value above 1 means stock price expcted to rise. </h6>
-    </div>
+    </div> */}
 
     <div style={{display:'flex'}} >
         <h6 style={{color:'red'}}> Chart </h6> <h6> &nbsp; Compares a few stocks, yearly_gain, zoom, date range, logarithmic. </h6>
-    </div>
-
-    <div style={{display:'flex'}} >
-        <h6 style={{color:'red'}}> Logarithmic graph </h6> <h6> &nbsp; - Facilitate comparison between crashes, like 2022 drop to 2008 drop. </h6>
     </div>
 
     <div style={{display:'flex'}} >
@@ -69,31 +51,41 @@ return (
         <h6 style={{color:'red'}}>Stocks preformed better than QQQ </h6> <h6> &nbsp; Year, 2Year, 5Year, 10Year (from select list)</h6>
     </div>
 
-    <div style={{display:'flex'}} >
+    {/* <div style={{display:'flex'}} >
         <h6 style={{color:'red'}}>Price / high </h6> <h6> &nbsp; Compare today price with high before deep. (How far from recovery) </h6>
-    </div>
+    </div> */}
 
-    <div style={{display:'flex'}} >
+    {/* <div style={{display:'flex'}} >
         <h6 style={{color:'red'}}>Data sharing </h6> <h6> &nbsp; Through a common Firebase </h6>
-    </div>
+    </div> */}
 
     <div style={{display:'flex'}} >
         <h6 style={{color:'red'}}>Monthly gain </h6> <h6> &nbsp; November has the best gain </h6>
     </div>
 
     <div style={{display:'flex'}} >
-        <h6 style={{color:'red'}}>Analyst target price </h6> <h6> &nbsp; Just started collecting, results analysis mot yet.  </h6>
+        <h6 style={{color:'red'}}>Analyst target price </h6> <h6> &nbsp; Just started collecting results.  </h6>
     </div>
+
+    {/* <div style={{display:'flex'}} >
+        <h6 style={{color:'red'}}> Target price </h6> <h6> &nbsp; - based an many analysts prediction. Value above 1 means stock price expcted to rise. </h6>
+    </div> */}
 
     <div style={{display:'flex'}} >
-        <h6> I base my selection on 2 factors: &nbsp; </h6><h6 style={{color:'red'}}>Long Term gain,
-        </h6><h6> &nbsp; and on  &nbsp;</h6><h6 style={{color:'red'}}> analist target price, prediction.  </h6>
+        <h6 style={{color:'red'}}> AlphVatage.co </h6> <h6> &nbsp; Analysis based on Historical stock prices</h6>
     </div>
-    <h6> Wisdom of the crowd </h6>
+    
+    <div style={{display:'flex'}}>
+        <h6 style={{color:'red'}}> Free sharing </h6> <h6>&nbsp; Pease share with anyone interested </h6>
+    </div>
+
     <hr/> 
+
+    {/* <h6> Wisdom of the crowd </h6> */}
+    {/* <hr/>  */}
     <h6>Source -  github.com/shagam/stock-chart </h6>
 
-    <h6>Written in ReactJS, on top of JavaScript.</h6> 
+    {/* <h6>Written in ReactJS, on top of JavaScript.</h6>  */}
     <hr/> 
     </div>
 
