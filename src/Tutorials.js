004aa99727d85c807093d13b928023af6395d920
eli shagam <eli.shagam@gmail.com>
2024-02-13 20:30:15 +0200

    Rearrange	modified:   src/Tutorials.js

--- src/Tutorials.js
+++ src/Tutorials.js
@@ -17,19 +17,25 @@ function Tutorials () {
 
             <hr/>
             <div style={{display: 'grid'}}>
-                <div><a href="https://youtu.be/y3CBXkZzSNs" >Quick PortFolio Check </a>  &nbsp; &nbsp;  &nbsp; Compare Stocks & ETF </div>
-                <div><a href="https://youtu.be/y3CBXkZzSNs" >Peak2Peak  </a>  &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;  &nbsp;         - long term yearly gain </div>
-                <div><a href="https://youtu.be/y3CBXkZzSNs" >Drop recovery analysis</a>  &nbsp;  2008, 2021, 2022</div>
-                <div><a href="https://youtu.be/y3CBXkZzSNs" >Common backend</a>  &nbsp; &nbsp;           get best stocks </div>
-                <div><a href="https://youtu.be/y3CBXkZzSNs" >analyst-target-price</a>  &nbsp; &nbsp;   history of target-price</div>
+                <div><a href="https://youtu.be/y3CBXkZzSNs" >Quick PortFolio Check</a>  &nbsp;  Compare Stocks & ETF </div>
+                <div><a href="https://youtu.be/y3CBXkZzSNs" >Common backend</a>  &nbsp; &nbsp;           Get best stocks </div>
+
+                <div><a href="https://youtu.be/y3CBXkZzSNs" >Peak2Peak  </a>  &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;          long term yearly gain </div>
+                <div><a href="https://youtu.be/y3CBXkZzSNs" >Drop recovery</a>  &nbsp; Analyse drops of 2008, 2021, 2022</div>
+
                 <div><a href="https://youtu.be/y3CBXkZzSNs" >Month gain</a>  &nbsp; &nbsp;  &nbsp; Months with highest gain </div> 
 
-                <div><a href="https://youtu.be/y3CBXkZzSNs" >Table</a>  &nbsp; &nbsp;  &nbsp; Compare many </div> 
-                <div><a href="https://youtu.be/y3CBXkZzSNs" >Chart</a>  &nbsp; &nbsp;  &nbsp; Compare few </div> 
+                <div><a href="https://youtu.be/y3CBXkZzSNs" >Table</a>  &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;   Compare many </div> 
+                <div><a href="https://youtu.be/y3CBXkZzSNs" >Chart</a>  &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;   Compare few </div> 
 
-                <div><a href="https://youtu.be/y3CBXkZzSNs" >Verify-1</a>  &nbsp; &nbsp;  &nbsp; Compare with MarketWatch historical price</div> 
-                <div><a href="https://youtu.be/y3CBXkZzSNs" >raw data</a>  &nbsp; &nbsp; Show raw data, as received </div>
-                
+                <div><a href="https://youtu.be/y3CBXkZzSNs" >Month gain</a>  &nbsp; &nbsp;  &nbsp; Months with highest gain </div> 
+                <div><a href="https://youtu.be/y3CBXkZzSNs" >Analyst-target-price</a>  &nbsp; &nbsp;   History of target-price</div>
+
+
+                <div><a href="https://youtu.be/y3CBXkZzSNs" >Raw data</a>  &nbsp; &nbsp;  &nbsp; &nbsp;Show raw data, as received </div>
+                <div><a href="https://youtu.be/y3CBXkZzSNs" >Persistance</a>  &nbsp; &nbsp; Stock info saved in localStorage </div>
+                <div><a href="https://youtu.be/y3CBXkZzSNs" >Verify-1</a>  &nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp; Compare with MarketWatch historical price</div> 
+
                 <hr/>
                 <a href="https://youtu.be/y3CBXkZzSNs" >English tutorial</a>  &nbsp; &nbsp; 
                 <a href="https://youtu.be/Rv5a0tkMISE" >Hebrew tutorial</a> &nbsp; &nbsp; 
