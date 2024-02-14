9d16473fa512e04f6b1b13ca280cfd6a83fc209d
eli shagam <eli.shagam@gmail.com>
2024-02-14 22:41:55 +0200

    remove spaces Fix warning	modified:   src/Tutorials.js

--- src/Tutorials.js
+++ src/Tutorials.js
@@ -8,63 +8,57 @@ function Tutorials () {
     return (
     <div>
 
-        <div>
-            <div className='w-100 text-left mt-2'>
-                <Link to="/" > Home </Link>
-            </div>
-            <hr/> 
-            <h4 style={{color:'Green'}}>Tutorials</h4>
+        <div className='w-100 text-left mt-2'>
+            <Link to="/" > Home </Link>
+        </div>
+        <hr/> 
+        <h4 style={{color:'Green'}}>Tutorials</h4>
 
-            <hr/>
-            <div style={{display: 'grid'}}>
-                <table>
+        {/* <hr/> */}
+        <div >
+            <table>
+                <tbody>
                     <tr>
-                        <td><a href="https://youtu.be/nywx9EncRo0" >Quick PortFolio Check</a></td> <td> Compare Stocks & ETF</td> 
+                        <td><a href="https://youtu.be/nywx9EncRo0">Quick PortFolio Check</a></td><td>Compare Stocks & ETF</td> 
                     </tr>
                     <tr>
-                        <td><a href="https://youtu.be/LwBOtJOIxco" >Peak2Peak  </a></td> <td>  long term yearly gain </td> 
+                        <td><a href="https://youtu.be/LwBOtJOIxco">Peak2Peak  </a></td><td>long term yearly gain </td> 
                     </tr>
                     <tr>
-                        <td><a href="https://youtu.be/MsJtdrBnLB8" >Drop recovery</a></td> <td> Analyse drops of 2008, 2021, 2022 </td> 
-                    </tr>
-
-                    {/* <div><a href="https://youtu.be/y3CBXkZzSNs" >Table</a>  &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;   Compare many </div>  */}
-
-                    <tr>
-                        <td><a href="https://youtu.be/2uUit4pmY7o" >Chart mechanizm</a></td> <td> Compare few stocks </td> 
-                    </tr>
-                    <tr>
-                        <td><a href="https://youtu.be/0sZ0QK1nwHo" >Month gain</a> </td> <td>Months with highest gain</td> 
-                    </tr>
-                    <tr>
-                        <td><a href="https://youtu.be/IElQqHTVPnE" >Analyst-target-price</a> </td> <td>History of target-price</td> 
-                    </tr>
-                    <tr>
-                        <td><a href="https://youtu.be/n_NXoU5ce-A" >Raw data</a>  </td> <td>Show raw data, as received</td> 
-                    </tr>
-                    <tr>
-                        <td><a href="https://youtu.be/cVxKLW3pPB0" >Persistance</a>  </td> <td>Stock info saved in localStorage</td> 
-                    </tr>
-                    <tr>
-                        <td><a href="https://youtu.be/IR3TBJlqM_g" >Verify</a>  </td> <td> Compare with MarketWatch historical price</td> 
+                        <td><a href="https://youtu.be/MsJtdrBnLB8">Drop recovery</a></td><td>Analyse drops of 2008, 2021, 2022 </td> 
                     </tr>
 
-                    <hr/>
+                    {/* <div><a href="https://youtu.be/y3CBXkZzSNs">Table</a>  &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;   Compare many </div>  */}
+
                     <tr>
-                        <td><a href="https://stocks-compare.netlify.app" >Protfolio Stocks compare</a>  </td> <td> Link to the tool</td> 
+                        <td><a href="https://youtu.be/2uUit4pmY7o">Chart mechanizm</a></td><td>Compare few stocks</td> 
                     </tr>
-                </table>
+                    <tr>
+                        <td><a href="https://youtu.be/0sZ0QK1nwHo">Month gain</a></td><td>Months with highest gain</td> 
+                    </tr>
+                    <tr>
+                        <td><a href="https://youtu.be/IElQqHTVPnE">Analyst-target-price</a></td><td>History of target-price</td> 
+                    </tr>
+                    <tr>
+                        <td><a href="https://youtu.be/n_NXoU5ce-A">Raw data</a></td><td>Show raw data, as received</td> 
+                    </tr>
+                    <tr>
+                        <td><a href="https://youtu.be/cVxKLW3pPB0">Persistance</a></td><td>Stock info saved in localStorage</td> 
+                    </tr>
+                    <tr>
+                        <td><a href="https://youtu.be/IR3TBJlqM_g">Verify</a></td><td>Compare with MarketWatch historical price</td> 
+                    </tr>
+                    <tr>
+                        <td><a href="https://stocks-compare.netlify.app">Protfolio Stocks compare</a></td><td>Link to the tool</td> 
+                    </tr>
+                </tbody>
+            </table>
 
-                {/* <a href="https://youtu.be/y3CBXkZzSNs" >English tutorial</a>  &nbsp; &nbsp;  */}
-                {/* <a href="https://youtu.be/Rv5a0tkMISE" >Hebrew tutorial</a> &nbsp; &nbsp;  */}
-                
-                <hr/>
-
-            </div>
-            <hr/> 
+            {/* <a href="https://youtu.be/y3CBXkZzSNs" >English tutorial</a>  &nbsp; &nbsp;  */}
+            {/* <a href="https://youtu.be/Rv5a0tkMISE" >Hebrew tutorial</a> &nbsp; &nbsp;  */}         
 
         </div>
-
+        <hr/> 
 
     </div>
     )}
