cc871f11a5c23cf2442e089356e8d80028bec594
eli shagam <eli.shagam@gmail.com>
2024-02-17 17:17:49 +0200

    Renove duplicate reloadPage from config	modified:   src/table/Config.js

--- src/table/Config.js
+++ src/table/Config.js
@@ -47,7 +47,7 @@ const  Config = (props) => {
           <div  style={{display:'flex'}}>
              <div> &nbsp; <button onClick={purgeStockTable} > Purge stock table </button> &nbsp; </div>
             <div> &nbsp; <button onClick={columnsHiddenPurge} > Default Hidden columns </button> &nbsp; </div>
-            <div> &nbsp; <button onClick={reloadPage} > Reload page </button> &nbsp; </div>
+            {/* <div> &nbsp; <button onClick={reloadPage} > Reload page </button> &nbsp; </div> */}
             <div> &nbsp; <button onClick={props.refreshByToggleColumns} > Refresh table </button> &nbsp;&nbsp; </div>
             <button style={{height: '30px'}} type="button" className="stock_button_class" onClick={()=>props.saveTable()}>saveTable &nbsp;   </button>
           </div>
