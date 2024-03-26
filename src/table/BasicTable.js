3eb0526905e798d030d74989928253cf02b997ff
eli shagam <eli.shagam@gmail.com>
2024-03-26 14:34:04 +0200

    URL Banner under table	modified:   src/table/Firebase.js

--- src/table/BasicTable.js
+++ src/table/BasicTable.js
@@ -1303,7 +1303,10 @@ const BasicTable = (props) => {
     </table>
        
     <div id='trailer_id'>
-
+        {eliHome && <div style={{display: 'flex'}}>
+          <h4 style={{'color':'blue'}}> Best stocks/ETF</h4>
+          <h4 style={{'color':'green'}}>&nbsp;&nbsp;(stocks-compare.netlify.app)</h4>
+        </div>}
         {chartSymbol && stockChartXValues.length > 0 && 
          <StockChart StockSymbol ={chartSymbol} stockChartXValues = {stockChartXValues}  stockChartYValues = {stockChartYValues}
           gainMap = {gainMap} isMobile = {isMobile} weekly = {weekly} logFlags = {props.logFlags} errorAdd = {errorAdd} bubbleLine = {bubbleLine}/>}
