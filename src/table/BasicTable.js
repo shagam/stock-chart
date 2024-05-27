0bb69ee46e4d77aa7f10ccff70845b7cd8e27e0d
eli shagam <eli.shagam@gmail.com>
2024-05-27 12:24:21 +0300

    add none radio button	modified:   src/table/BasicTable.js

--- src/table/BasicTable.js
+++ src/table/BasicTable.js
@@ -1243,6 +1243,7 @@ const BasicTable = (props) => {
           </div>
           {<div>
             <div style={{display:'flex'}}>
+              <input style={{marginLeft: '0px'}}  type="radio" name="mon" value='none' id='7' checked={analyzeTool==='none'} onChange={onOptionChange}/> <div style={{color:'red'}}>none</div> &nbsp;
               <input style={{marginLeft: '0px'}}  type="radio" name="mon" value='dropRecovery' id='0' checked={analyzeTool==='dropRecovery'} onChange={onOptionChange}/> dropRecovery          
               <input style={{marginLeft: '5px'}}  type="radio" name="mon" value='peak2peak' id='1' checked={analyzeTool==='peak2peak'} onChange={onOptionChange}/> peak2peak
               <input style={{marginLeft: '5px'}}  type="radio" name="mon" value='holdings' id='5' checked={analyzeTool==='holdings'} onChange={onOptionChange}/> holdings
