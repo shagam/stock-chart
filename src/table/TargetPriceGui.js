3257227818b23a56606d12ce308ddcce2dae0a40
eli shagam <eli.shagam@gmail.com>
2024-05-28 10:04:31 +0300

    Pull TargetPriceGui.js out of Tools.js	new file:   src/table/TargetPriceGui.js 	modified:   src/table/Tools.js

---
+++ src/table/TargetPriceGui.js
@@ -0,0 +1,87 @@
+import React, {useState, useEffect} from 'react'
+import {getTargetPriceArray, targetHistAll, targetHistoryAll, moveFromFirebase} from './TargetPrice'
+import IpContext from '../contexts/IpContext';
+
+function TargetPriceGui (props) {
+    const [targetInfo, setTargetInfo] = useState ();
+    const [targetPriceHist, setTargetPriceHist] = useState ({});
+
+    const [price, setPrice] = useState ();
+    const [target, setTarget] = useState ();
+    const {localIp, localIpv4, eliHome} = IpContext();
+
+    const LOG = props.logFlags.includes('month')
+
+    // clear vars when symbol change
+    useEffect(() => {
+        setTargetInfo()
+        setPrice()
+
+        // setTargetPriceHist()
+    },[props.symbol]) 
+
+    function targetGetOne () {
+        const row_index = props.rows.findIndex((row)=> row.values.symbol === props.symbol);
+        if (row_index  === -1)
+            return;
+        if (props.rows[row_index].values.PE === -2) {
+            props.errorAdd([props.symbol, 'no target price for ETF'])
+            return;
+        }
+        const tar = getTargetPriceArray (props.symbol, setTargetInfo, props.logFlags, props.errorAdd, props.ssl, props.PORT, props.servSelect)
+        setPrice (props.rows[row_index].values.price) // for display
+        setTarget (props.rows[row_index].values.target) // for display
+        }
+    
+        function targetGetAll () {
+        const tar = targetHistAll (setTargetPriceHist, props.logFlags, props.errorAdd, props.ssl, props.PORT, props.servSelect)
+        }
+      
+        // show as vertical list of array items
+        function renderList(array) {
+            if (array.length < 1)
+            return;
+            if (array[0].date)
+            return array.map((item) => <li key={item.date}>{JSON.stringify(item)}</li>);
+            else
+            return array.map((item) => <li>{JSON.stringify(item)}</li>);  
+        }
+  
+
+    return (
+
+        <div>
+
+            <h3>Target price history</h3>
+
+            {false && eliHome && <button type="button" onClick={()=>moveFromFirebase (props.logFlags, props.errorAdd, props.ssl, props.PORT, props.servSelect)}>moveFromFirebase</button>}  &nbsp; &nbsp;
+
+            <button type="button" onClick={()=>targetGetAll ()}>targetHistoryAll</button>  &nbsp; &nbsp;
+            
+            {props.symbol && <button type="button" onClick={()=>targetGetOne ()}>targetHistoryOne </button> }
+            {target && price && <div>price: {price} &nbsp; &nbsp; target: {target}  &nbsp; &nbsp; (target above 1 - means growth) </div> }
+            {price && targetInfo && renderList(targetInfo)}
+
+            <br></br>           
+
+            {targetPriceHist && Object.keys(targetPriceHist).length > 0 &&
+            <div  style={{display: 'flex'}}>count={Object.keys(targetPriceHist).length} &nbsp; &nbsp; &nbsp; <div style={{color: 'lightGreen'}}>(targetNew &nbsp;/ targetOld)</div> </div>}
+
+            <div  style={{ maxHeight: '65vh', 'overflowY': 'scroll'}}  > 
+            {targetPriceHist && Object.keys(targetPriceHist).length > 0 && Object.keys(targetPriceHist).sort().map((sym,i)=>{
+            return (
+                <div style={{width: '90vw'}} key={i}>
+                    <div  style={{display: 'flex'}} >
+                    <div style={{color: 'red', width: '50px'}} > {sym}   </div>   ({targetPriceHist[sym].length}) &nbsp; &nbsp;
+                    <div style={{color: 'lightGreen'}} > {(targetPriceHist[sym][targetPriceHist[sym].length - 1].target /  targetPriceHist[sym][0].target).toFixed(3)} </div>
+                    </div> 
+                    {targetPriceHist[sym].map((targetItem) => <li key={targetItem.date}>{JSON.stringify(targetItem)} </li>)} 
+                </div>
+                )
+            })}
+            </div>
+        </div>
+    )
+}
+
+export {TargetPriceGui}
--- src/table/Tools.js
+++ src/table/Tools.js
@@ -1,9 +1,9 @@
 import React, {useState, useEffect} from 'react'
 
-import {getTargetPriceArray, targetHistAll, targetHistoryAll, moveFromFirebase} from './TargetPrice'
 
-import {todaySplit, todayDate, todayDateSplit, dateSplit, monthsBack, daysBack, compareDate, daysFrom1970, 
-    searchDateInArray, monthsBackTest, daysBackTest, getDate, getDateSec, dateStr} from '../utils/Date'
+
+// import {todaySplit, todayDate, todayDateSplit, dateSplit, monthsBack, daysBack, compareDate, daysFrom1970, 
+//     searchDateInArray, monthsBackTest, daysBackTest, getDate, getDateSec, dateStr} from '../utils/Date'
 
 import {format} from "date-fns"
 import DatePicker, {moment} from 'react-datepicker';
@@ -12,19 +12,14 @@ import "react-datepicker/dist/react-datepicker.css";
 import logFlags from '../LogFlags'
 import {monthGain} from './MonthGain'
 import IpContext from '../contexts/IpContext';
+import { TargetPriceGui } from './TargetPriceGui';
 
 function Tools (props) {
     const [displayFlag, setDisplayFlag] = useState (false); 
 
     const {localIp, localIpv4, eliHome} = IpContext();
+
     const [mGainObj, setMgainObj] = useState ({});
-    
-    const [targetInfo, setTargetInfo] = useState ();
-    const [targetPriceHist, setTargetPriceHist] = useState ({});
-
-    const [price, setPrice] = useState ();
-    const [target, setTarget] = useState ();
-
     const [yearGain, setYearGain] = useState ();
     const [monthNames, setMonthNames] = useState(['Jan','Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
 
@@ -39,40 +34,11 @@ function Tools (props) {
 
   // clear vars when symbol change
     useEffect(() => {
-        setTargetInfo()
-        setPrice()
+
         setMgainObj({})
-        // setTargetPriceHist()
       },[props.symbol]) 
   
-  
-  
-    function targetGetOne (symbol) {
-      const tar = getTargetPriceArray (props.symbol, setTargetInfo, props.logFlags, props.errorAdd, props.ssl, props.PORT, props.servSelect)
-  
-      const row_index = props.rows.findIndex((row)=> row.values.symbol === props.symbol);
-      if (row_index  === -1)
-        return;
-      setPrice (props.rows[row_index].values.price) // for display
-      setTarget (props.rows[row_index].values.target) // for display
-    }
-
-    function targetGetAll () {
-      const tar = targetHistAll (setTargetPriceHist, props.logFlags, props.errorAdd, props.ssl, props.PORT, props.servSelect)
-    }
-
-
-
-    // show as vertical list of array items
-    function renderList(array) {
-      if (array.length < 1)
-        return;
-      if (array[0].date)
-        return array.map((item) => <li key={item.date}>{JSON.stringify(item)}</li>);
-      else
-        return array.map((item) => <li>{JSON.stringify(item)}</li>);  
-    }
-    
+      
     return (
         <div style = {{border: '2px solid blue'}} >
        
@@ -96,38 +62,11 @@ function Tools (props) {
 
                 {Object.keys(mGainObj).length > 0 && <div>stockCount={Object.keys(props.gainMap).length} yearlyGain={yearGain.toFixed(3)} </div>}
                 <br></br>           
-                
-                 <h3>Target price history</h3>
-
-                {false && eliHome && <button type="button" onClick={()=>moveFromFirebase (props.logFlags, props.errorAdd, props.ssl, props.PORT, props.servSelect)}>moveFromFirebase</button>}  &nbsp; &nbsp;
-
-                <button type="button" onClick={()=>targetGetAll ()}>targetHistoryAll</button>  &nbsp; &nbsp;
-                
-                {props.symbol && <button type="button" onClick={()=>targetGetOne ()}>targetHistoryOne </button> }
-                {target && price && <div>price: {price} &nbsp; &nbsp; target: {target}  &nbsp; &nbsp; (target above 1 - means growth) </div> }
-                {price && targetInfo && renderList(targetInfo)}
-            
-                <br></br>           
-        
-                {targetPriceHist && Object.keys(targetPriceHist).length > 0 &&
-                 <div  style={{display: 'flex'}}>count={Object.keys(targetPriceHist).length} &nbsp; &nbsp; &nbsp; <div style={{color: 'lightGreen'}}>(targetNew &nbsp;/ targetOld)</div> </div>}
-
-                <div  style={{ maxHeight: '65vh', 'overflowY': 'scroll'}}  > 
-                {targetPriceHist && Object.keys(targetPriceHist).length > 0 && Object.keys(targetPriceHist).sort().map((sym,i)=>{
-                  return (
-                      <div style={{width: '90vw'}} key={i}>
-                        <div  style={{display: 'flex'}} >
-                          <div style={{color: 'red', width: '50px'}} > {sym}   </div>   ({targetPriceHist[sym].length}) &nbsp; &nbsp;
-                          <div style={{color: 'lightGreen'}} > {(targetPriceHist[sym][targetPriceHist[sym].length - 1].target /  targetPriceHist[sym][0].target).toFixed(3)} </div>
-                        </div> 
-                        {targetPriceHist[sym].map((targetItem) => <li key={targetItem.date}>{JSON.stringify(targetItem)} </li>)} 
-                      </div>
-                    )
-                })}
-                </div>
-
-              <br></br>         
+                    
             </div>
+
+            <TargetPriceGui  symbol = {props.symbol} rows = {props.rows} logFlags = {props.logFlags} errorAdd={props.errorAdd} gainMap = {props.gainMap}
+             ssl={props.ssl} PORT={props.PORT} servSelect={props.servSelect}/>
         </div>
     )
 
