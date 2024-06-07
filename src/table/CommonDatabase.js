2da4d28d469165d6c21916970dbaa533e9c9225a
eli shagam <eli.shagam@gmail.com>
2024-06-07 08:15:52 +0300

    Change txt commonDatabase gainWrite	modified:   src/table/CommonDatabase.js

--- src/table/CommonDatabase.js
+++ src/table/CommonDatabase.js
@@ -85,7 +85,7 @@ function GainWrite (sym, rows, setError, corsServer, PORT, ssl, logFlags) {
     // delete dat.verifyPrice
 
     // if (LOG)
-    console.log ('gainWrite', dat1)
+    console.log ('CommonDatabase gainWrite', dat1)
 
     corsUrl += corsServer+ ":" + PORT + "/gain?cmd=w&stock=" + sym + '&factor=1.1' + '&dat=' + JSON.stringify(dat1); 
 
