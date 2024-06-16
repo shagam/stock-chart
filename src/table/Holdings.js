cb3c688364810d0198d6ebb5771557bcb4a21e51
eli shagam <eli.shagam@gmail.com>
2024-06-16 17:58:46 +0300

    Change layout	modified:   src/table/Holdings.js

--- src/table/Holdings.js
+++ src/table/Holdings.js
@@ -261,18 +261,21 @@ function Holdings (props) {
         <h6 style={{color: 'blue'}}> ETF Holdings &nbsp;  </h6>
       </div>
 
+      <div>&nbsp; </div>
       {/* <br></br> */}
       {/* ====== Buttons */} 
       {props.chartSymbol && <div>
           <div stype={{display: 'flex'}}>
-              {eliHome &&  <input type="checkbox" checked={ignoreSaved}  onChange={setIgnore}  />  } &nbsp;IgnoreSaved &nbsp; &nbsp;
-              <GetInt init={count} callBack={setCount} title='Count-Limit (50 max) &nbsp;' type='Number' pattern="[0-9]+"/> 
-              <button style={{background: 'aqua'}} type="button" onClick={()=>fetchHoldings (0)}>fetch50  </button> &nbsp; 
-              {props.eliHome && <button style={{background: 'aqua'}} type="button" onClick={()=>fetchHoldings (1)}>fetch20  </button>} &nbsp;
-              {/* <button type="button" onClick={()=>fetchHoldings (2)}>fetch10  </button> &nbsp; */}
+            <button type="button" onClick={()=>togglePercent ()}>toggle % column  </button>  &nbsp; &nbsp;
+            {eliHome &&  <input type="checkbox" checked={ignoreSaved}  onChange={setIgnore}  />} &nbsp;IgnoreSaved &nbsp; &nbsp;
+            <GetInt init={count} callBack={setCount} title='Count-Limit (50 max) &nbsp;' type='Number' pattern="[0-9]+"/> 
+          </div>
 
-              {holdingsRawObj[props.chartSymbol] && <button type="button" onClick={()=>holdingsInsertInTable ()}>insert-in-table &nbsp; {props.chartSymbol} </button> } &nbsp;
-              <button type="button" onClick={()=>togglePercent ()}>togglePercentColumn  </button>
+          <div>  
+            <button style={{background: 'aqua'}} type="button" onClick={()=>fetchHoldings (0)}>fetch50  </button> &nbsp; 
+            {props.eliHome && <button style={{background: 'aqua'}} type="button" onClick={()=>fetchHoldings (1)}>fetch20  </button>} &nbsp;
+            {/* <button type="button" onClick={()=>fetchHoldings (2)}>fetch10  </button> &nbsp; */}
+            {holdingsRawObj[props.chartSymbol] && <button type="button" onClick={()=>holdingsInsertInTable ()}>insert-in-table &nbsp; {props.chartSymbol} </button> } &nbsp;
           </div> 
           <div>&nbsp; </div>
         </div>
