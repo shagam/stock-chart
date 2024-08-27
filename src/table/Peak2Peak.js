bfeae46333ba755217c1a6aa7937cd7a8c421b56
eli shagam <eli.shagam@gmail.com>
2024-08-27 11:54:46 +0300

    Change button text	modified:   src/table/Peak2Peak.js

--- src/table/Peak2Peak.js
+++ src/table/Peak2Peak.js
@@ -127,7 +127,7 @@ const Peak2PeakGui = (props) => {
               <button style={{background: 'aqua'}} type="button" onClick={()=>peak2PeakCalc (props.symbol, props.rows, props.stockChartXValues, props.stockChartYValues,
                props.weekly, props.logFlags, props.searchPeak, startDate, endDate, props.errorAdd, setResults, props.saveTable)}>Calc peak2peak gain </button> &nbsp; &nbsp;
 
-              {results && ! props.bubleLine && <button style={{background: 'aqua'}} type="button"  onClick={() => {calcBaseLine (props.stockChartXValues, props.stockChartYValues)}}>  Bubble baseLine </button>}
+              {results && ! props.bubleLine && <button style={{background: 'aqua'}} type="button"  onClick={() => {calcBaseLine (props.stockChartXValues, props.stockChartYValues)}}> calc Bubble-Line </button>}
               {bubbleLineFlag && <div style={{color: 'magenta'}} >{props.symbol} currentPrice / bubbleLine = {bubbleLineRatio} </div>}
            </div>
            
