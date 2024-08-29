17fc465b7653a596744f26039bd11835d653dd6c
eli shagam <eli.shagam@gmail.com>
2024-08-29 15:48:53 +0300

    Add combobox still have refresh problems	modified:   src/table/StockLists.js

--- src/table/StockLists.js
+++ src/table/StockLists.js
@@ -1,17 +1,36 @@
 import React, {useState} from 'react'
 import GlobalFilter from '../utils/GlobalFilter'
 import {addStock} from './AddStock'
+import { ComboBoxSelect } from '../utils/ComboBoxSelect'
 
 function StockLists (props) {
+    const [displayFlag, setDisplayFlag] = useState(false);
+
     const [listName, setListName] = useState();
     const [stockLists, setStockLists] = useState({});
-    const [displayFlag, setDisplayFlag] = useState(false);
+
+    const [names, setNames] = useState([]);
+
+    const LOG = props.logFlags && props.logFlags.includes("stockLists");
+
 
     const keys = Object.keys(stockLists);
     if (keys.length === 0)
         get()
 
-    const LOG = props.logFlags && props.logFlags.includes("stockLists");
+    function buildListSelect(stockListLocal) {
+        // if (stockLists === {})
+        //     return;
+        if (names.length !== 0)
+            return
+        const l = Object.keys(stockListLocal) 
+        if (l.length === 0)
+            return;
+        // if (LOG )
+            console.log ('build list=', l);         
+        setNames(l)
+    }
+
 
     function add() {
         const symbols = [];
@@ -34,14 +53,18 @@ function StockLists (props) {
 
         stockLists[listName] = symbols; 
         setStockLists(stockLists)
+        buildListSelect(stockLists)
         localStorage.setItem('stocksLists', JSON.stringify(stockLists))
     }
 
     function get () {
+        // if (LOG)
+            console.log('get')
         const listsRaw = localStorage.getItem('stocksLists')
         if (listsRaw) {
-            const lists = JSON.parse(listsRaw)
-            setStockLists(lists)
+            const stockListLocal = JSON.parse(listsRaw)
+            setStockLists(stockListLocal)
+            buildListSelect(stockListLocal)
         }
     }
 
@@ -57,6 +80,7 @@ function StockLists (props) {
         }
         else
             console.log ('missing list=', listName)
+        buildListSelect(stockLists)
     }
 
     function insert() {
@@ -68,8 +92,10 @@ function StockLists (props) {
         const list = stockLists[listName];
         for (let i = 0; i < list.length; i++)
             addStock(props.rows, list[i], false)
+        // buildListSelect()
         props.saveTable('stockLists');  
         window.location.reload();
+        buildListSelect(stockLists)
     }
 
     return (
@@ -90,12 +116,19 @@ function StockLists (props) {
                 <div style={{display: 'flex'}}>
                     <div style={{padding: '14px'}}>List-name</div>
                     <GlobalFilter className="stock_button_class_" filter={listName} setFilter={setListName} name='listName' isMobile={false}/>
-                    &nbsp; &nbsp; <button onClick={add} > addLIst </button>  
+                    &nbsp; &nbsp; <button onClick={add} > addNewList </button>  
                     {/* &nbsp; &nbsp; <button onClick={get} > get </button>   */}
+
+                </div>
+                <div style={{display:'flex'}}>
+                    <div style={{display:'flex'}}> <ComboBoxSelect serv={names} nameList={names} setSelect={setListName} title='Choose-list' options={names} defaultValue={names[0]}/> </div>
                     &nbsp; &nbsp; <button onClick={del} > delete </button>  
                     &nbsp; &nbsp; <button onClick={insert} > insertInTable </button>  
                 </div>
-                <pre>{JSON.stringify(stockLists, null, 2)}</pre>
+
+                <pre> {JSON.stringify(names, null, 2)}</pre>
+                <pre> {JSON.stringify(stockLists, null, 2)}</pre>
+                
           </div>}
       </div>
 
