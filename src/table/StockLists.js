18df39a01f1aa6369a0b4e369dd630c8aeeead22
eli shagam <eli.shagam@gmail.com>
2024-08-29 18:37:57 +0300

    good version	modified:   src/table/StockLists.js

--- src/table/StockLists.js
+++ src/table/StockLists.js
@@ -5,14 +5,19 @@ import { ComboBoxSelect } from '../utils/ComboBoxSelect'
 
 function StockLists (props) {
     const [displayFlag, setDisplayFlag] = useState(false);
-
+    const [version, setVersion] = useState(0)
     const [listName, setListName] = useState();
+    const [newListName, setNewListName] = useState();
     const [stockLists, setStockLists] = useState({});
 
-    const [names, setNames] = useState([]);
+    const [nameArray, setNameArray] = useState([]);
 
     const LOG = props.logFlags && props.logFlags.includes("stockLists");
 
+    function refresh() {
+        // window.location.reload();
+        setVersion(version+1)
+    }
 
     const keys = Object.keys(stockLists);
     if (keys.length === 0)
@@ -21,14 +26,19 @@ function StockLists (props) {
     function buildListSelect(stockListLocal) {
         // if (stockLists === {})
         //     return;
-        if (names.length !== 0)
+        if (nameArray.length !== 0)
             return
-        const l = Object.keys(stockListLocal) 
-        if (l.length === 0)
-            return;
+
+        const nameArrayLocal = Object.keys(stockListLocal) 
         // if (LOG )
-            console.log ('build list=', l);         
-        setNames(l)
+        console.log ('before build array=', nameArray); 
+       
+        if (nameArrayLocal.length === 0)
+            return;
+       
+        setNameArray(nameArrayLocal.sort())
+        console.log ('after build array=', nameArrayLocal); 
+        setListName(nameArrayLocal[0])
     }
 
 
@@ -39,9 +49,9 @@ function StockLists (props) {
             symbols.push (sym)
         }
        
-        console.log (listName, symbols)
+        console.log (newListName, symbols)
 
-        if (! listName) {
+        if (! newListName) {
             alert ('Missing list Name')
             return;
         }
@@ -51,10 +61,14 @@ function StockLists (props) {
         if (keys.length === 0)
             get()
 
-        stockLists[listName] = symbols; 
+        stockLists[newListName] = symbols;
+        const nameArrayLocal = Object.keys(stockLists)
+        setNameArray(nameArrayLocal)
+        setStockLists({})
         setStockLists(stockLists)
-        buildListSelect(stockLists)
+        // buildListSelect(stockLists)
         localStorage.setItem('stocksLists', JSON.stringify(stockLists))
+        refresh()
     }
 
     function get () {
@@ -64,6 +78,8 @@ function StockLists (props) {
         if (listsRaw) {
             const stockListLocal = JSON.parse(listsRaw)
             setStockLists(stockListLocal)
+            // const nameArrayLocal = Object.keys(stockLists)
+            // setNameArray(nameArrayLocal)
             buildListSelect(stockListLocal)
         }
     }
@@ -77,10 +93,13 @@ function StockLists (props) {
             delete stockLists[listName]
             localStorage.setItem('stocksLists', JSON.stringify(stockLists))
             setStockLists(stockLists)
+            const nameArrayLocal = Object.keys(stockLists)
+            setNameArray(nameArrayLocal)
+            setNewListName()
         }
         else
             console.log ('missing list=', listName)
-        buildListSelect(stockLists)
+        refresh()
     }
 
     function insert() {
@@ -90,12 +109,16 @@ function StockLists (props) {
         }
 
         const list = stockLists[listName];
+        if (! list) {
+            console.log ('err', listName)
+        }
         for (let i = 0; i < list.length; i++)
             addStock(props.rows, list[i], false)
-        // buildListSelect()
+
         props.saveTable('stockLists');  
+        const nameArrayLocal = Object.keys(stockLists)
+        setNameArray(nameArrayLocal)
         window.location.reload();
-        buildListSelect(stockLists)
     }
 
     return (
@@ -108,25 +131,22 @@ function StockLists (props) {
             </div>
 
             { displayFlag && <div>
-                <div style = {{display: 'flex'}}>
-                    <div  style={{color: 'magenta' }}>  {props.infoSymbol} </div> &nbsp; &nbsp;
-                    {/* <h6  style={{color: 'blue' }}> Stock-lists </h6> */}
-                </div>
-
+                {version}
                 <div style={{display: 'flex'}}>
-                    <div style={{padding: '14px'}}>List-name</div>
-                    <GlobalFilter className="stock_button_class_" filter={listName} setFilter={setListName} name='listName' isMobile={false}/>
-                    &nbsp; &nbsp; <button onClick={add} > addNewList </button>  
-                    {/* &nbsp; &nbsp; <button onClick={get} > get </button>   */}
+                    {/* <div style={{padding: '14px'}}>List-name</div> */}
+                    <button style={{hight: '8px' }} onClick={add} > addNewList </button>   &nbsp; &nbsp;
+                    <GlobalFilter className="stock_button_class_" filter={newListName} setFilter={setNewListName} name='newListName' isMobile={false}/>
 
+                    {/* &nbsp; &nbsp; <button onClick={get} > get </button>   */}
                 </div>
+                <div> &nbsp; </div>
                 <div style={{display:'flex'}}>
-                    <div style={{display:'flex'}}> <ComboBoxSelect serv={names} nameList={names} setSelect={setListName} title='Choose-list' options={names} defaultValue={names[0]}/> </div>
+                    <div style={{display:'flex'}}> <ComboBoxSelect serv={nameArray} nameList={nameArray} setSelect={setListName} title='Choose-list' options={nameArray} defaultValue={nameArray[0]}/> </div>
                     &nbsp; &nbsp; <button onClick={del} > delete </button>  
                     &nbsp; &nbsp; <button onClick={insert} > insertInTable </button>  
                 </div>
 
-                <pre> {JSON.stringify(names, null, 2)}</pre>
+                <pre> {JSON.stringify(nameArray, null, 2)}</pre>
                 <pre> {JSON.stringify(stockLists, null, 2)}</pre>
                 
           </div>}
