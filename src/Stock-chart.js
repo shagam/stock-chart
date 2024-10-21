8fea236bbc1cf44be69eaf2e94c6460c97e85a56
eli <eli.shagam@gmail.com>
2024-10-21 22:08:06 +0300

    Add checkbox <static>	modified:   src/Stock-chart.js

--- src/Stock-chart.js
+++ src/Stock-chart.js
@@ -26,6 +26,7 @@ const StockChart = (props) => {
   const [chartFlag, setChartFlag] = useState(first); // hide / show page
   const [multi, setMulti] = useState(true);
   const [showRanges, setShowRanges] = useState();
+  const [static_, setStatic] = useState();
 
   const [logarithmic, setLogarithmic] = useState(false);
   const [scaleFlag, setScaleFlag] = useState(! props.gainMap['bubbleLine']);
@@ -397,7 +398,9 @@ const StockChart = (props) => {
           <div> <input  type="checkbox" checked={multi}  onChange={() => setMulti (! multi)} />  multi </div>
           <div> &nbsp;&nbsp;&nbsp; <input  type="checkbox" checked={logarithmic}  onChange={() => setLogarithmic (! logarithmic)} />  Logarithemic &nbsp;&nbsp;  </div>
           {gainChart.length > 1 && <div>  <input  type="checkbox" checked={scaleFlag}  onChange={() => setScaleFlag (! scaleFlag)} /> scale &nbsp; </div>}  &nbsp; 
+          <input  type="checkbox" checked={static_}  onChange={() => setStatic (! static_)} />&nbsp;static &nbsp;&nbsp;
           <input  type="checkbox" checked={showRanges}  onChange={() => setShowRanges (! showRanges)} /> &nbsp;ranges  &nbsp;
+
           {showRanges && <div>
             &nbsp; &nbsp;
             <button onClick={() => setDateRange(new Date(1999,0,1), new Date(2008,0,1))}> 1999-2008</button>&nbsp;
@@ -413,7 +416,7 @@ const StockChart = (props) => {
         <div id = 'chart_id'>
         {/* yaxis={'title': 'x-axis','fixedrange':True, 'autorange': false},
        yaxis={'title': 'y-axis','fixedrange':True}) */}
-          {(os==='iPhone' || os ==='Android') && <Plot  data={gainChart} 
+          {(props.isMobile || static_) && <Plot  data={gainChart} 
             layout={{ width: 700, height: 350, title: title, staticPlot: true, yaxis: {fixedrange: false}  }}
              config={{staticPlot: true, 'modeBarButtonsToRemove': ['zoom','zoomOut','zoomIn','pan']}} />}
           {! props.isMobile && <Plot  data={gainChart} 
