823d01fd4c2e28b5452355e7d7efa6c087945912
eli <eli.shagam@gmail.com>
2025-01-16 10:44:05 +0200

    Still unused	modified:   src/utils/Server.js

--- src/utils/Server.js
+++ src/utils/Server.js
@@ -5,28 +5,28 @@ import { AuthProvider, useAuth } from '../contexts/AuthContext';
 
 const PORT = 5000
 
-var server
+var servSelect
 const ssl = true;
 function ServerSelect () {
     const {localIp, localIpv4, eliHome, city, countryName, countryCode, regionName, ip, os} = IpContext();
    
     const servList = [process.env.REACT_APP_AWS_IP, process.env.REACT_APP_LOCAL_SERV_IP];
     const servNameList = ['production', 'test'];
-    const [servSelect, setServSelect] = useState(servList[0]);
+    const [serv, setServSelect] = useState(servList[0]);
     const [ssl, setSsl] = useState(true)
     const { login, currentUser, admin } = useAuth();
 
    
 
-    server = servSelect;
+    servSelect = serv;
     return (
         <div>
-            {eliHome && <div style={{display:'flex'}}> <ComboBoxSelect serv={servSelect} nameList={servNameList} setSelect={setServSelect} 
-            title='backEndServer' options={servList} defaultValue={servSelect}/> </div>}            
+            {eliHome && <div style={{display:'flex'}}> <ComboBoxSelect serv={serv} nameList={servNameList} setSelect={setServSelect} 
+            title='backEndServer' options={servList} defaultValue={serv}/> </div>}            
 
             {admin && <div> &nbsp; <input  type="checkbox" checked={ssl}  onChange={() => {setSsl (! ssl)}} /> ssl &nbsp;&nbsp;</div>} 
         </div>
     )
 }
 
-export {ServerSelect, server, PORT, ssl}
+export {ServerSelect, servSelect, PORT, ssl}
