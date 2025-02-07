e139a29472504fd2b0255eceb2d4adee74048476
eli <eli.shagam@gmail.com>
2025-02-07 08:26:58 +0200

    Missing price: click latPrice 	modified:   src/table/TargetPriceGui.js

--- src/table/TargetPriceGui.js
+++ src/table/TargetPriceGui.js
@@ -122,6 +122,7 @@ function TargetPriceGui (props) {
                 {targetInfoOne && props.symbol && <button style={{height: '35px', marginTop: '7px'}} type="button" onClick={()=>checkPrediction ()}>checkPrediction </button> }
             </div>
 
+            {!price && targetInfoOne && targetInfoOne.length > 0 && <div style={{color: 'red'}}> Click lastPrice at the top && click targetHistoryOne again </div>}
             {target && price && <div>price: {price} &nbsp; &nbsp; target: {target}  &nbsp; &nbsp; (target above 1 - means growth) </div> }
             
             {targetInfoOne && targetInfoOne.length > 0 && <div>count={targetInfoOne && targetInfoOne.length}</div>}
