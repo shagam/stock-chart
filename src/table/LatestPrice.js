8a98aa2ba4ffa55d2f8f813d2ff524f6eca69ef1
eli <eli.shagam@gmail.com>
2025-03-18 08:28:05 +0200

    add date to log 	modified:   src/table/Finnhub.js 	modified:   src/table/LatestPrice.js

--- src/table/LatestPrice.js
+++ src/table/LatestPrice.js
@@ -122,8 +122,8 @@ function LatestPrice (props) {
           const ratio = price / props.stockChartYValues[0];
           const sign = ratio > 1 ? '+' : '' 
           const color = ratio > 1 ? '#82b74b': 'red' 
-          console.log (props.symbol, 'price=' + price, ' highest=' + highestPrice.toFixed(2), ' price/high=' + (price / highestPrice).toFixed(3), 'closePrice=' + props.stockChartYValues[0],
-           'price/close=' + (price / props.stockChartYValues[0]).toFixed(4), 'color:' + color)
+          console.log (props.symbol, getDate(), 'price=' + price, ' highest=' + highestPrice.toFixed(2), ' price/high=' + (price / highestPrice).toFixed(3), 'closePrice=' + props.stockChartYValues[0],
+           'price/close=' + (price / props.stockChartYValues[0]).toFixed(4))
           const priceDivCloseObj = {symbol: props.symbol, price: price, sign: sign, ratio: ((price/props.stockChartYValues[0] -1) * 100).toFixed(3), color: color, seconds: result.data.secondsDiff.toFixed(0)};
 
           setPriceDivClose (priceDivCloseObj)
