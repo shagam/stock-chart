46a17fc9f3b6a71c5886ad4577956aaf42a3b14a
eli <eli.shagam@gmail.com>
2025-03-19 23:19:04 +0200

    Add tutorials bubbleLine, dropRecovery	modified:   src/Tutorials.js

--- src/Tutorials.js
+++ src/Tutorials.js
@@ -41,15 +41,11 @@ function Tutorials () {
                     {! language && <tr>
                         <td><a href="https://youtu.be/jibwwWP0OVQ">Introduction </a></td><td>Compare Stocks & ETF (PortFolio Check)</td> 
                     </tr>}                 
-                    {/* "https://youtu.be/AMrNSLRRuxY" */}
                     {! language && <tr>
-                        <td><a href="https://youtu.be/aLYaMStQVds">Peak2Peak</a></td><td>How to measure long term yearly gain of an ETF</td> 
+                        <td><a href="https://youtube.com/shorts/29TnD1h8ZPs">Bubble-line </a></td><td>Stock market bubble. Can we predict future crash? </td> 
                     </tr>}
                     {! language && <tr>
-                        <td><a href="https://youtu.be/xXB-dzEKAus">Bubble-line </a></td><td>Stock market bubble. Can we predict future crash? </td> 
-                    </tr>}
-                    {! language && <tr>
-                        <td><a href="https://youtu.be/F458c7qZx2k">Drop-recovery</a></td><td>Analyse drops and recovery 2008, 2020, 2022 </td> 
+                        <td><a href="https://youtube.com/shorts/C0UjQWHWbB8">Drop-recovery</a></td><td>Analyse drops and recovery 2008, 2020, 2022 </td> 
                     </tr>}
                     {! language && <tr>
                         <td><a href="https://youtu.be/Yk6fj_peDlM">ETF-Holdings</a></td><td>ETF holdings review and compare ETFs</td> 
