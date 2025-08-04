b719fa3acc9f3c0107a3c70e40df2d310dcac419
shagam <eli.shagam@gmail.com>
2025-08-04 08:49:19 +0300

    prepare for search/filter	modified:   src/Tutorials.js

--- src/Tutorials.js
+++ src/Tutorials.js
@@ -3,6 +3,242 @@ import { Link, useNavigate } from 'react-router-dom'
 import MobileContext from './contexts/MobileContext'
 import { ComboBoxSelect } from './utils/ComboBoxSelect'
 import {IpContext, getIpInfo} from './contexts/IpContext';
+import GetInt from './utils/GetInt'
+
+
+const language = false
+const tutorialArray = [
+    {
+        condition: ! language,
+        link:         'https://youtube.com/shorts/29TnD1h8ZPs',
+        name:         'Bubble-line',
+        description:  'Stock market bubble. Can we predict future crash? ',
+    },
+    // {
+    //     condition: language,
+    //     link:         'https://youtu.be/Rv5a0tkMISE',
+    //     name:         'Hebrew tutorial',
+    //     description:  "",
+    // },
+    {
+        condition: ! language,
+        link:         'https://youtube.com/shorts/C0UjQWHWbB8',
+        name:         'Drop-recovery',
+        description:  'Analyse drops and recovery 2008, 2020, 2022 ',
+    },
+    {
+        condition: ! language,
+        link:         'https://youtube.com/shorts/jlFlJfKPGrE',
+        name:         'ETF-Holdings',
+        description:  'ETF holdings review and compare ETFs',
+    },
+    {
+        condition: ! language,
+        link:         'https://youtube.com/shorts/hjxdWTGohAc',
+        name:         'Common DataBase',
+        description:  'Stocks with 2x gain higher than QQQ: 1Yr, 2Yr, 5Yr, 10Yr',
+    },
+    {
+        condition: ! language,
+        link:         'https://youtube.com/shorts/zQdRjpuYkjA',
+        name:         'Month-gain',
+        description:  "Is there a systematic difference between month's gain",
+    },
+    {
+        condition: ! language,
+        link:         'https://youtube.com/shorts/ziRLU3XPJRY?feature=share',
+        name:         'Trade-simulator',
+        description:  "Can we beat the gain of QQQ, just by trading QQQ?",
+    },
+    {
+        condition: true,
+        link:         null,
+        name:         '',
+        description:  <div>&nbsp;</div>,
+    },
+    {
+        condition: ! language,
+        link:         'https://youtube.com/shorts/12oKOXv0gqA',
+        name:         'Table-view',
+        description:  "Compare many stocks & ETF, (sort/filter/select column)",
+    },
+    {
+        condition: ! language,
+        link:         'https://youtube.com/shorts/11RweD4QKnI',
+        name:         'Chart-view',
+        description:  "Compare a few stocks",
+    },
+    {
+        condition: true,
+        link:         null,
+        name:         '',
+        description:  <div>&nbsp;</div>,
+    },
+    {
+        condition: ! language,
+        link:         'https://youtube.com/shorts/AK38qRao-TE',
+        name:         'Price-alert (group)',
+        description:  "Compare selected stocks closing price with highest price",
+    },
+    {
+        condition: ! language,
+        link:         'https://youtube.com/shorts/9n_nQuhbeFc',
+        name:         'Price-alert (specific stock)',
+        description:  "Compare price of stock with threshold ",
+    },
+    {
+        condition: ! language,
+        link:         'https://youtube.com/shorts/UOtcApd5XOY',
+        name:         'Rise-drop-frequency',
+        description:  "How often is there a 5%, price rise or drop?",
+    },
+    {
+        condition: ! language,
+        link:         'https://youtube.com/shorts/h2sE1njL4iw',
+        name:         'Moving-average',
+        description:  "May be used for predicting market trend",
+    },
+    {
+        condition: ! language,
+        link:         'https://youtube.com/shorts/jSlOyfLPMZc',
+        name:         'CandleSticks',
+        description:  "Popular prediction mechanism",
+    },
+    {
+        condition: ! language,
+        link:         'https://youtube.com/shorts/FgYGr5KcJe0',
+        name:         'Analyst-target-price',
+        description:  "History of analyst-target-price prediction (average of many)",
+    },
+    {
+        condition: true,
+        link:         null,
+        name:         '',
+        description:  <div>&nbsp;</div>,
+    },
+    {
+        condition: ! language,
+        link:         'https://youtube.com/shorts/S_eBgij72Zk',
+        name:         'stock-lists',
+        description:  "Save & Share stock-list between users",
+    },
+    {
+        condition: ! language,
+        link:         'https://youtube.com/shorts/w0qQc_Ue_jw',
+        name:         'Verify_1',
+        description:  "Verify historical price. Compare with other sites",
+    },
+    {
+        condition: ! language,
+        link:         'https://youtube.com/shorts/BpC7wK7Psvw',
+        name:         'Raw data',
+        description:  "Raw historical stock prices, received from AlphaVantage",
+    },
+    {
+        condition: ! language,
+        link:         'https://youtube.com/shorts/k5CbJD8cIAA',
+        name:         'Persistance',
+        description:  "Stock info saved in localStorage",
+    },
+    {
+        condition: true,
+        link:         null,
+        name:         '',
+        description:  <div>&nbsp;</div>,
+    },
+    {
+        condition: ! language,
+        link:         'https://youtube.com/shorts/rxrcge4LsIc',
+        name:         'Berkshire-Hathaway',
+        description:  "BRK-B gain compared with S&P anf QQQ",
+    },
+    {
+        condition: ! language,
+        link:         'https://youtube.com/shorts/6tbGbdvEvt0',
+        name:         'Leverage-hightech-etf',
+        description:  "Short term profit example, using tripple-hitech",
+    },
+    {
+        condition: ! language,
+        link:         'https://youtube.com/shorts/6F06FI6mdG8',
+        name:         'Is QQQ better than SPY',
+        description:  "During the last year - same holdings for both",
+    },
+    {
+        condition: ! language,
+        link:         'https://youtube.com/shorts/ul_LzUeENd4',
+        name:         'Market-crash-hint',
+        description:  "Bubble-line proximety can hint for market crash",
+    },
+    {
+        condition: true,
+        link:         null,
+        name:         '',
+        description:  <div>&nbsp;</div>,
+    },
+    {
+        condition: ! language,
+        link:         'https://youtube.com/playlist?list=PLBwPhbE_paAyMgNmnnkCjUYloLcAIy6ks&feature=shared',
+        name:         'Tutorial Playlist',
+        description:  "Serialized tutorials",
+    },
+    {
+        condition: true,
+        link:         null,
+        name:         '',
+        description:  <div>&nbsp;</div>,
+    },
+    {
+        condition: language,
+        link:         'https://youtu.be/Rv5a0tkMISE',
+        name:         'Hebrew tutorial',
+        description:  "",
+    },
+    {
+        condition: false && language,
+        link:         'https://www.veed.io/view/df28ebf3-2a6c-4ac8-81f4-1ab9ca3f33c4?panel=share',
+        name:         'Bubble-line (Heb)',
+        description:  " קו-הבועות בעברית",
+    },
+    {
+        condition: language,
+        link:         'https://www.veed.io/edit/e848e95a-0131-49b4-9114-5c0f3f9c19c0/video',
+        name:         'Laverage ETF (Heb)',
+        description:  "Short term gain trading TQQQ (Hebrew)",
+    },
+    {
+        condition: true,
+        link:         null,
+        name:         '',
+        description:  <div>&nbsp;</div>,
+    },
+    {
+        condition: language,
+        link:         'https://stocks-compare.netlify.app',
+        name:         'Stocks-compare',
+        description:  "Link for the tool",
+    },
+
+
+]
+
+//                         return <tr key={index} style={{display: item.condition ? 'table-row' : 'none'}}>
+
+                //  <tbody> 
+                //       {tutorialArray.map((item, index) => {
+                //         return <tr key={index} >
+                //             <td>
+                //                 {item.link ? <a href={item.link}><b>{item.name}</b> </a> : <td>{item.description}</td>}
+                //             </td>
+                //             <td>{item.description}</td>
+                //         </tr>
+
+                //       })}
+                //     <tr>
+                //        <div>&nbsp;</div>
+                //     </tr> 
+                // </tbody>
+
 
 function Tutorials () {
     const {eliHome} = IpContext();
@@ -10,6 +246,7 @@ function Tutorials () {
     const {isMobile} = MobileContext();
     const [mobile, setMobile]  = useState(isMobile);
     const [language, setLanguage] = useState(false);
+    const [searchText, setSearchText] = useState('');
     // https://www.w3schools.com/cssref/pr_class_display.php
 
     const languageList = ['English', 'Hebrew']
@@ -29,140 +266,25 @@ function Tutorials () {
 
             {/* {eliHome && <div style={{display:'flex'}}> <ComboBoxSelect serv={language} nameList={languageList} setSelect={setLanguage} title='backEnd' options={languageList} defaultValue={language}/> &nbsp;&nbsp;</div>}  */}
             <div style={{display: 'flex'}}>
-                <ComboBoxSelect serv={language} nameList={languageList} setSelect={setLanguage} title='' TITLE='Choose language ' options={[false,true]} defaultValue={false} /> &nbsp;
+                <ComboBoxSelect serv={language} nameList={languageList} setSelect={setLanguage} title='' TITLE='Choose language ' options={[false,true]} defaultValue={false} /> &nbsp; &nbsp; &nbsp;&nbsp;
                 {/* <ComboBoxSelect serv={mobile} nameList={platformList} setSelect={setMobile} title='' TITLE='Mobile or desktop ' options={[false,true]} defaultValue={false} /> &nbsp; */}
+                {eliHome && <GetInt init={searchText} callBack={setSearchText} title='search/Filter  &nbsp;' type='text' pattern="[0-9_a-zA-Z\\.]+" width = '35%'/>}
+                <hr/> 
             </div>
         </div>         
 
         {/* <hr/> */}
         <div >
             <table>
-                <tbody>              
-                    {/* {! language && <tr>
-                        <td><a href="https://youtu.be/jibwwWP0OVQ">Introduction </a></td><td>Compare Stocks & ETF (PortFolio Check)</td> 
-                    </tr>}                  */}
-                    {! language && <tr>
-                        <td><a href="https://youtube.com/shorts/29TnD1h8ZPs"><b>Bubble-line</b> </a></td><td>Stock market bubble. Can we predict future crash? </td> 
-                    </tr>}
-                    {! language && <tr>
-                        <td><a href="https://youtube.com/shorts/C0UjQWHWbB8"><b>Drop-recovery</b></a></td><td>Analyse drops and recovery 2008, 2020, 2022 </td> 
-                    </tr>}
-                    {! language && <tr>
-                        <td><a href="https://youtube.com/shorts/jlFlJfKPGrE"><b>ETF-Holdings</b></a></td><td>ETF holdings review and compare ETFs</td> 
-                    </tr>}
-
-                    {! language && <tr>
-                        <td><a href="https://youtube.com/shorts/hjxdWTGohAc"><b>Common DataBase</b>  </a></td><td>
-                             Stocks with 2x gain higher than QQQ: 1Yr, 2Yr, 5Yr, 10Yr</td> 
-                    </tr>}
-                    {! language && <tr>
-                        <td><a href="https://youtube.com/shorts/zQdRjpuYkjA"><b>Month-gain</b></a></td><td>Is there a systematic difference between month's gain</td> 
-                    </tr>}
-
-                    {! language && <tr>
-                        <td><a href="https://youtube.com/shorts/ziRLU3XPJRY?feature=share"><b>Trade-simulator</b></a></td><td>Can we beat the gain of QQQ, just by trading QQQ?</td> 
-                    </tr>}
-
-                    <tr>
-                       <div>&nbsp;</div>
-                    </tr>           
-                           
-                    {! language && <tr>
-                        <td><a href="https://youtube.com/shorts/12oKOXv0gqA"><b>Table-view</b></a></td><td>Compare many stocks & ETF, (sort/filter/select column)</td> 
-                    </tr>}
-                    {! language && <tr>
-                        <td><a href="https://youtube.com/shorts/11RweD4QKnI"><b>Chart-view</b></a></td><td>Compare a few stocks</td> 
-                    </tr>}
-                    
-                    <div>&nbsp;</div>
-                    {! language && <tr>
-                        <td><a href="https://youtube.com/shorts/AK38qRao-TE"><b>Price-alert (group)</b></a></td><td>Compare selected stocks closing price with highest price</td> 
-                    </tr>}
-                    {! language && <tr>
-                        <td><a href="https://youtube.com/shorts/9n_nQuhbeFc"><b>Price-alert (specific stock)</b></a></td><td>Compare price of stock with threshold </td> 
-                    </tr>}                 
-                    {! language && <tr>
-                        <td><a href="https://youtube.com/shorts/UOtcApd5XOY"><b>Rise-drop-frequency</b></a></td><td>How often is there a 5%, price rise or drop?</td> 
-                    </tr>}
-                    {! language && <tr>
-                        <td><a href="https://youtube.com/shorts/h2sE1njL4iw"><b>Moving-average</b></a></td><td>May be used for predicting market trend</td> 
-                    </tr>}
-                    {! language && <tr>
-                        <td><a href="https://youtube.com/shorts/jSlOyfLPMZc"><b>CandleSticks</b></a></td><td>Popular prediction mechanism</td> 
-                    </tr>}
-                    {! language && <tr>
-                        <td><a href="https://youtube.com/shorts/FgYGr5KcJe0"><b>Analyst-target-price</b></a></td><td>History of analyst-target-price prediction (average of many)</td> 
-                    </tr>}
-
-                    
-                    <div>&nbsp;</div>
-                    {! language && <tr>
-                        <td><a href="https://youtube.com/shorts/S_eBgij72Zk"><b>stock-lists</b></a></td><td>Save & Share stock-list between users</td> 
-                    </tr>}
-                    {! language && <tr>
-                        <td><a href="https://youtube.com/shorts/w0qQc_Ue_jw"><b>Verify_1</b></a></td><td>Verify historical price. Compare with other sites</td> 
-                    </tr>}
-                          {! language && <tr>
-                        <td><a href="https://youtube.com/shorts/BpC7wK7Psvw"><b>Raw data</b></a></td><td>Raw historical stock prices, received from AlphaVantage</td> 
-                    </tr>}
-                    {! language && <tr>
-                        <td><a href="https://youtube.com/shorts/k5CbJD8cIAA"><b>Persistance</b></a></td><td>Stock info saved in localStorage</td> 
-                    </tr>}
-
-                    <tr>
-                       <div>&nbsp;</div>
-                    </tr>
-
-                    {! language && <tr>
-                        <td><a href="https://youtube.com/shorts/rxrcge4LsIc"><b>Berkshire-Hathaway</b> </a></td><td>BRK-B gain compared with S&P anf QQQ</td> 
-                    </tr>}                    
-                    {! language && <tr>
-                        <td><a href="https://youtube.com/shorts/6tbGbdvEvt0"><b>Leverage-hightech-etf</b> </a></td><td>Short term profit example . </td> 
-                    </tr>}
-                    {/* {! language && <tr>
-                        <td><a href="https://youtube.com/shorts/lMb-a7BA_ws"><b>Leverage_ETF</b></a></td><td>Short term gain on Leverage_ETF like TQQQ</td> 
-                    </tr>} */}
-                    {! language && <tr>
-                        <td><a href="https://youtube.com/shorts/6F06FI6mdG8"><b>Is QQQ better than SPY</b></a></td><td>During the last year - same holdings for both</td> 
-                    </tr>}
-
-                    {! language && <tr>
-                        <td><a href="https://youtube.com/shorts/ul_LzUeENd4"><b>Market-crash-hint</b> </a></td><td>Bubble-line proximety can hint for market crash</td> 
-                    </tr>}
-
-                    
-                    
-
-                    <tr>
-                       <div>&nbsp;</div>
-                    </tr>
-                    {! language && <tr>
-                        <td><a href="https://youtube.com/playlist?list=PLBwPhbE_paAyMgNmnnkCjUYloLcAIy6ks&feature=shared"> <b>Tutorial Playlist</b></a></td><td>Serialized tutorials</td> 
-                    </tr>}
-
-                    {<tr>
-                       <div>&nbsp;</div>
-                    </tr>}
-                    {/* {language &&  <tr>
-                        <td><a href="https://youtu.be/Rv5a0tkMISE">Hebrew tutorial</a></td><td></td>        
-                    </tr>} */}
-
-
-
-                    {eliHome && language && <tr>
-                        <td><a href="https://www.veed.io/view/df28ebf3-2a6c-4ac8-81f4-1ab9ca3f33c4?panel=share"><b>Bubble-line (Heb)</b></a></td> קו-הבועות בעברית<td></td>        
-                    </tr>}     
-                    {language && <tr>
-                        <td><a href="https://www.veed.io/edit/e848e95a-0131-49b4-9114-5c0f3f9c19c0/video"><b>Laverage ETF (Heb)</b></a></td> Short term gain trading TQQQ (Hebrew)<td></td>        
-                    </tr>}                
-
-                    <tr>
-                       <div>&nbsp;</div>
-                    </tr>                    
-                    <tr>
-                        <td><a href="https://stocks-compare.netlify.app">Stocks-compare</a></td><td>Link for the tool</td> 
-                    </tr>
-
+                <tbody> 
+                      {tutorialArray.map((item, index) => {
+                        return item.condition && <tr key={index} >
+                            <td>
+                                { <a href={item.link}><b>{item.name}</b> </a>}
+                            </td>
+                            <td>{item.description}</td>
+                        </tr>
+                      })}
                 </tbody>
             </table>
 
