fbe1942e68dcf3a021e46ae284b4a1699252f253
shagam <eli.shagam@gmail.com>
2025-10-14 23:17:40 +0300

    Add commente clear button 	modified:   src/utils/News.js

--- src/utils/News.js
+++ src/utils/News.js
@@ -30,15 +30,17 @@ function News (props) {
     <div>
         {(props.eliHome && news.optionCount < 8) && <div style={{display: 'flex', border:'2px solid magenta'}}>
             <hr/> 
-            <div> New support page for  <strong style={{color:'green'}}>Call options</strong> &nbsp; &nbsp; </div>
+            <div> &nbsp; New support page for  <strong style={{color:'green'}}>Call options</strong> &nbsp; &nbsp; </div>
             <a href="https://youtube.com/shorts/YnNDCs4sUyA" target="_blank" rel="noopener noreferrer">
                 <b>Call Option Tutorial</b>
             </a>
             {/* { <a href={'https://youtube.com/shorts/YnNDCs4sUyA'}><b>'call option tutorial'</b> </a>} */}
 
             {news_Option ()}
+
             <hr/> 
-        </div>}   
+        </div>} 
+        {/* {props.eliHome && <button onClick={() => localStorage.removeItem(NEWS)}> clear </button> }   */}
     </div>
   )
 }
