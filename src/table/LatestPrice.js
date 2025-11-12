14062c90c11bcb8bfc042ed09c78da109c9f4374
shagam <eli.shagam@gmail.com>
2025-11-12 19:44:06 +0200

    use marketData.app for stock LatestPrice	modified:   src/table/LatestPrice.js

--- src/table/LatestPrice.js
+++ src/table/LatestPrice.js
@@ -22,24 +22,8 @@ function LatestPrice (props) {
 
     const log = props.logFlags.includes('gain')
     const [priceDivClose, setPriceDivClose] = useState()
-  
-    // if (req.query.src === 'goog'){
-    //     url = 'https://www.google.com/finance/quote/' + req.query.stock + ':NASDAQ'
-    //     pattern = 'data-currency-code="USD" data-last-price="([0-9.]+)'
-    // }
-    // else if (req.query.src === 'nasdaq') {
-    //     url = 'https://www.nasdaq.com/market-activity/etf/' + req.query.stock
-    //         // <bg-quote class="value" field="Last" format="0,0.00" channel="/zigman2/quotes/208575548/composite,/zigman2/quotes/208575548/lastsale" session="pre">513.26</bg-quote>
-    //     pattern = '<bg-quote class="value" field="Last" format="0,0.00" channel="/zigman2/quotes/208575548/composite,/zigman2/quotes/208575548/lastsale" session="pre">([0-9.]+)</bg-quote>'
-    // }
-    // else if (req.query.src === 'yahoo') {
-    //     url = 'https://finance.yahoo.com/quote/' + req.query.stock + '/'
-    //     pattern = 'data-testid="qsp-pre-price">([0-9.]+)'
-    // }
-    // else if (req.query.src === 'watch') {
-    //     url = 'https://www.marketwatch.com/investing/fund/' + req.query.stock 
-    //     pattern = 'data-last-raw="([0-9.]+)">([0-9.]+)</bg-quote>'
-    // }
+    const [price, setPrice] = useState()
+    const [changepct, setChangepct] = useState()
 
     const openInNewTab = (url) => {
         window.open(url, "_blank", "noreferrer");
@@ -61,125 +45,79 @@ function LatestPrice (props) {
         // props.allColumns[ind_price].toggleHidden();
         props.errorAdd([props.symbol, 'Column nonVisible. price or priceDivHigh. use column_select to make visible']) // show error if priceDivHigh is visible
       } 
-      // if (! isInvisible_priceDivHigh){
-      //   // props.allColumns[ind_priceDivHigh].toggleHidden(); 
-      //   props.errorAdd([props.symbol, ' priceDivHigh column nonVisible. use column_select to turn on visible']) // show error if priceDivHigh is visible
-      // }
     }
 
-    function extendedHoursPrice () {
+    function getPrice() {
+      const TOKEN = process.env.REACT_APP_MARKETDATA;
+      var url = 'https://api.marketdata.app/v1/stocks/quotes/' + props.symbol
+      url += '/?token=' + TOKEN
+      axios.get (url)
+      .then ((result) => {
+        if (result.data.s !== 'ok') {
+          console.log (props.symbol, 'option-fee error', result.data.s)
+          return 'fail status =' + result.data.s 
+        }
+      
+        const priceInfo = result.data
+        console.log (result.data)
+      
+      const price_ = priceInfo.mid[0]; 
+      setPrice(price_)
+      const changepct_ = priceInfo.changepct[0]
+      setChangepct(changepct_ * 100)
+      console.log (price_, changepct_)
 
-        const u =  'https://www.barchart.com/etfs-funds/quotes/' + props.symbol + '/overview/'
-        // p:  '"lastPriceExt":"538.15","priceChangeExt":',},
-        const p =   '"lastPriceExt":"([0-9\\.]*)","priceChangeExt":'
+      var highestPrice = -1; // highest price
+      if (props.stockChartYValues.length > 0)
+      for (let i = 0; i < props.stockChartYValues.length; i++) {
+          const val = props.stockChartYValues[i];
+          if (val > highestPrice)
+              highestPrice = val;
+      } 
 
-        // console.log ('urlGetParse', serverUrl, pattern, LOG, callBack) 
-        var url
-        if (props.ssl) 
-          url = "https://";
-        else 
-            url = "http://"; 
-  
-        url += props.servSelect + ":" + props.PORT + "/urlGetParse?stock=" + props.symbol + '&url=' + u + '&pattern=' + p
-  
-        if (log)
-          url += '&LOG=true';
-        
-        if (ignoreSaved)
-            url += '&ignoreSaved=true';
+      const row_index = props.rows.findIndex((row)=> row.values.symbol === props.symbol);
 
-        if (log)
-          console.log (u, p) // log the url
-        props.setErr ()
-        setLatency('request sent to server')
-        const mili = Date.now()
-        // setResults('waiting for response')
-        axios.get (url)
-        .then ((result) => {
-
-            if (JSON.stringify(result.data).includes ('err Request failed')) {
-                props.errorAdd([props.symbol, ' latestPrice', result.data])
-                // props.setErr (result.data)
-                return
-            }
-  
-          const latency = Date.now() - mili
-          setLatency('response latency(msec)=' + latency)
-          if (log)
-              console.log ('result=', result.data)
-          if (result.data && result.data === 'read ETIMEDOUT'){ //} || result.data+''.includes('fail')) {
-              props.setError([props.symbol + ' LatestPrice ' + result.data])
-            //   setResults()
-              return;
-          }
-  
-          const dat = result.data
-          if (dat && typeof dat === 'string' && dat.startsWith('fail')) {
-              console.log(props.symbol, getDate(), 'latestPrice', dat)
-              props.setErr(props.symbol + ' ' +  getDate() + ' latestPrice ' + dat)
-              return;
-          }
-        //   if (log)
-        //       console.log (dat)
-          // find highest price
-          var highestPrice = -1; // highest price
-          for (let i = 0; i < props.stockChartYValues.length; i++) {
-              const val = props.stockChartYValues[i];
-              if (val > highestPrice)
-                  highestPrice = val;
-          } 
-
-          const price = Number(result.data.result_1)
-
-        //   console.log (getDate(), result.data)
-          const row_index = props.rows.findIndex((row)=> row.values.symbol === props.symbol);
-
-          props.rows[row_index].values.price = price.toFixed(2);
-          props.rows[row_index].values.price_mili = Date.now();
-          props.rows[row_index].values.priceDivHigh = (price / highestPrice).toFixed(4);
-          props.setPrice(price)
-          if (! props.stockChartYValues[0]) {
-              props.setErr('price not found, Try again')
-              return;
-          }
-          const ratio = price / props.stockChartYValues[0];
-          const sign = ratio > 1 ? '+' : '' 
-          const color = ratio > 1 ? '#82b74b': 'red' 
-          console.log (props.symbol, getDate(), 'price=' + price, ' highest=' + highestPrice.toFixed(2), ' price/high=' + (price / highestPrice).toFixed(3), 'closePrice=' + props.stockChartYValues[0],
-           'price/close=' + (price / props.stockChartYValues[0]).toFixed(4))
-          const priceDivCloseObj = {symbol: props.symbol, price: price, sign: sign, ratio: ((price/props.stockChartYValues[0] -1) * 100).toFixed(3), color: color, seconds: result.data.secondsDiff.toFixed(0)};
-
-          setPriceDivClose (priceDivCloseObj)
-          if (false)
-            checkPriceColumnVisible()
-          props.refreshByToggleColumns()
-          props.setErr()
-
-        })
-        .catch ((err) => {
-            props.errorAdd([props.symbol, 'LatestPrice ', err.message])
-            console.log(getDate(), 'msg', err, url)
-        })  
-    }
+      props.rows[row_index].values.price = price_.toFixed(2);
+      props.rows[row_index].values.price_mili = Date.now();
+      props.rows[row_index].values.priceDivHigh = (price_ / highestPrice).toFixed(4);
+      props.setPrice(price_)
 
 
-//  'inline-block'
+      const ratio = price_ / props.stockChartYValues[0];
+      const sign = changepct_ >= 0 ? '+' : '' 
+      const color = changepct_ >= 0 ? '#82b74b': 'red' 
+      console.log (props.symbol, getDate(), 'price=' + price_, ' highest=' + highestPrice.toFixed(2), ' price/high=' + (price_ / highestPrice).toFixed(3), 'closePrice=' + props.stockChartYValues[0],
+        'price/close=' + (price_ / props.stockChartYValues[0]).toFixed(4))
+      const priceDivCloseObj = {symbol: props.symbol, price: price_, sign: sign, ratio: (changepct_), color: color};
+      console.log (priceDivCloseObj)
+      if ( highestPrice !== -1)
+        setPriceDivClose (priceDivCloseObj)
+      else
+        setPriceDivClose('Press again for diff')
+      if (false)
+        checkPriceColumnVisible()
+      props.refreshByToggleColumns()
+      props.setErr()
+    })
+    .catch ((err) => {
+      console.log (err.message)
+      return ('fail latest price of ' + props.symbol+ ' ' + err.message)
+    })
+
+  }
+
+
+
     return (
         <div style={{display: 'flex'}}>
-            {props.symbol}&nbsp;
-            {/* {eliHome && props.symbol && <button style={{height:'28px'}} onClick={() => openInNewTab('https://www.barchart.com/etfs-funds/quotes/' + props.symbol)}> barchart </button>} &nbsp; */}
-            {/* {eliHome && props.symbol && <button style={{height:'28px'}} onClick={() => openInNewTab('https://www.marketwatch.com/investing/fund/' + props.symbol)}> marketWatch </button>} &nbsp; */}
+                      
+            <button  style={{background: 'aqua', height:'28px'}} type="button" title="price during market closed (not ready)" onClick={()=>getPrice()}>price </button> &nbsp;
             
-            {props.symbol && <div><button style={{backgroundColor: 'aqua', height:'28px'}} onClick={() => {finnhub (props.symbol, props.stockChartYValues, props.rows, props.refreshByToggleColumns, props.setErr,
-            props.logFlags, props.errorAdd, props.ssl, props.PORT, props.servSelect, setPriceDivClose, props.eliHome, checkPriceColumnVisible, props.setPrice)}} title='price during market open' > marketOpen </button> </div>}
+            {/* {priceDivClose && <div style={{display: 'flex'}} >&nbsp;&nbsp;{priceDivClose.symbol}&nbsp; <div style={{color: priceDivClose.color}}> {priceDivClose.sign}{priceDivClose.ratio}% </div> &nbsp;({priceDivClose.price})</div>} */}
+        
+            {priceDivClose && <div style={{display: 'flex'}} >&nbsp;&nbsp;{props.symbol}&nbsp; &nbsp;{price} &nbsp; <div style={{color: changepct >= 0 ? '#82b74b': 'red'}}> 
+              {changepct>0 ? '+': ''}{changepct}% </div> </div>}
 
-            {/* <ComboBoxSelect serv={source} nameList={priceSources} setSelect={setSource} title='' TITLE='market open price ' options={priceSources} defaultValue={false} /> &nbsp; */}
-            {/* <div> <input  type="checkbox" checked={subPages}  onChange={()=> setSubPages(! subPages)} />  subPages </div> */}
-            &nbsp;<button  style={{background: 'aqua', height:'28px'}} type="button" title="price during market closed (not ready)" onClick={()=>extendedHoursPrice()}>marketClosed </button> &nbsp;
-            
-            {props.eliHome && <div style={{display: 'flex'}}><input  type="checkbox" checked={ignoreSaved}  onChange={() => setIgnoreSaved (! ignoreSaved)} />&nbsp;ignoreSaved</div>} &nbsp;
-            
-            {priceDivClose && <div style={{display: 'flex'}} >&nbsp;&nbsp;{priceDivClose.symbol}&nbsp; <div style={{color: priceDivClose.color}}> {priceDivClose.sign}{priceDivClose.ratio}% </div> &nbsp;({priceDivClose.price})</div>}
         </div>
 
     )
