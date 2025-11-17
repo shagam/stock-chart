8e36d0fdbc4aa14318f4475c6f03951db826d3e2
shagam <eli.shagam@gmail.com>
2025-11-17 07:06:44 +0200

    put back marketClosed button 	modified:   src/table/LatestPrice.js

--- src/table/LatestPrice.js
+++ src/table/LatestPrice.js
@@ -47,6 +47,103 @@ function LatestPrice (props) {
       } 
     }
 
+  function extendedHoursPrice () {
+
+        const u =  'https://www.barchart.com/etfs-funds/quotes/' + props.symbol + '/overview/'
+        // p:  '"lastPriceExt":"538.15","priceChangeExt":',},
+        const p =   '"lastPriceExt":"([0-9\\.]*)","priceChangeExt":'
+
+        // console.log ('urlGetParse', serverUrl, pattern, LOG, callBack) 
+        var url
+        if (props.ssl) 
+          url = "https://";
+        else 
+            url = "http://"; 
+  
+        url += props.servSelect + ":" + props.PORT + "/urlGetParse?stock=" + props.symbol + '&url=' + u + '&pattern=' + p
+  
+        if (log)
+          url += '&LOG=true';
+        
+        if (ignoreSaved)
+            url += '&ignoreSaved=true';
+
+        if (log)
+          console.log (u, p) // log the url
+        props.setErr ()
+        setLatency('request sent to server')
+        const mili = Date.now()
+        // setResults('waiting for response')
+        axios.get (url)
+        .then ((result) => {
+
+            if (JSON.stringify(result.data).includes ('err Request failed')) {
+                props.errorAdd([props.symbol, ' latestPrice', result.data])
+                // props.setErr (result.data)
+                return
+            }
+  
+          const latency = Date.now() - mili
+          setLatency('response latency(msec)=' + latency)
+          if (log)
+              console.log ('result=', result.data)
+          if (result.data && result.data === 'read ETIMEDOUT'){ //} || result.data+''.includes('fail')) {
+              props.setError([props.symbol + ' LatestPrice ' + result.data])
+            //   setResults()
+              return;
+          }
+  
+          const dat = result.data
+          if (dat && typeof dat === 'string' && dat.startsWith('fail')) {
+              console.log(props.symbol, getDate(), 'latestPrice', dat)
+              props.setErr(props.symbol + ' ' +  getDate() + ' latestPrice ' + dat)
+              return;
+          }
+        //   if (log)
+        //       console.log (dat)
+          // find highest price
+          var highestPrice = -1; // highest price
+          for (let i = 0; i < props.stockChartYValues.length; i++) {
+              const val = props.stockChartYValues[i];
+              if (val > highestPrice)
+                  highestPrice = val;
+          } 
+
+          const price = Number(result.data.result_1)
+
+        //   console.log (getDate(), result.data)
+          const row_index = props.rows.findIndex((row)=> row.values.symbol === props.symbol);
+
+          props.rows[row_index].values.price = price.toFixed(2);
+          props.rows[row_index].values.price_mili = Date.now();
+          props.rows[row_index].values.priceDivHigh = (price / highestPrice).toFixed(4);
+          props.setPrice(price)
+          setPrice(price)
+          if (! props.stockChartYValues[0]) {
+              props.setErr('price not found, Try again')
+              return;
+          }
+          const ratio = price / props.stockChartYValues[0];
+          setChangepct(((ratio -1) * 100).toFixed(3))
+          const sign = ratio > 1 ? '+' : '' 
+          const color = ratio > 1 ? '#82b74b': 'red' 
+          console.log (props.symbol, getDate(), 'price=' + price, ' highest=' + highestPrice.toFixed(2), ' price/high=' + (price / highestPrice).toFixed(3), 'closePrice=' + props.stockChartYValues[0],
+           'price/close=' + (price / props.stockChartYValues[0]).toFixed(4))
+          const priceDivCloseObj = {symbol: props.symbol, price: price, sign: sign, ratio: ((price/props.stockChartYValues[0] -1) * 100).toFixed(3), color: color, seconds: result.data.secondsDiff.toFixed(0)};
+
+          setPriceDivClose (priceDivCloseObj)
+          if (false)
+            checkPriceColumnVisible()
+          props.refreshByToggleColumns()
+          props.setErr()
+
+        })
+        .catch ((err) => {
+            props.errorAdd([props.symbol, 'LatestPrice ', err.message])
+            console.log(getDate(), 'msg', err, url)
+        })  
+    }
+
 
     // function extendedHoursPrice () {
 
@@ -151,6 +248,7 @@ function LatestPrice (props) {
       const TOKEN = process.env.REACT_APP_MARKETDATA;
       var url = 'https://api.marketdata.app/v1/stocks/quotes/' + props.symbol
       url += '/?token=' + TOKEN
+      url += '&extended=true'
       axios.get (url)
       .then ((result) => {
         if (result.data.s !== 'ok') {
@@ -216,7 +314,7 @@ function LatestPrice (props) {
             <button  style={{background: 'aqua', height:'28px'}} type="button" title="price during market closed (not ready)" onClick={()=>getPrice()}>price </button> &nbsp;
             
             {/* {priceDivClose && <div style={{display: 'flex'}} >&nbsp;&nbsp;{priceDivClose.symbol}&nbsp; <div style={{color: priceDivClose.color}}> {priceDivClose.sign}{priceDivClose.ratio}% </div> &nbsp;({priceDivClose.price})</div>} */}
-             {/* &nbsp;<button  style={{background: 'aqua', height:'28px'}} type="button" title="price during market closed (not ready)" onClick={()=>extendedHoursPrice()}>marketClosed </button> &nbsp; */}
+             {props.eliHome && <button  style={{background: 'aqua', height:'28px'}} type="button" title="price during market closed (not ready)" onClick={()=>extendedHoursPrice()}>marketClosed </button>};
  
             {priceDivClose && <div style={{display: 'flex'}} >&nbsp;&nbsp;{priceDivClose.symbol}&nbsp; &nbsp;{price} &nbsp; <div style={{color: changepct >= 0 ? '#82b74b': 'red'}}> 
               {changepct>0 ? '+': ''}{changepct}% </div> </div>}
