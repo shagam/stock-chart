b6c2430d613ccc2bba50c8fc12b7575e714b861f
shagam <eli.shagam@gmail.com>
2026-08-15 21:00:01 +0300

    Change text adjust expectedPrice	modified:   src/table/StockOptionsConfig.js

--- src/table/StockOptionsConfig.js
+++ src/table/StockOptionsConfig.js
@@ -111,7 +111,7 @@ function StockOptionsConfig (props) {
           <ComboBoxSelect serv={action} nameList={actionList} setSelect={setAction} title='action=' options={actionList} defaultValue={action}/> &nbsp; &nbsp;  &nbsp; 
         </div>
         <br/> 
-        <label style={{margin: '0px', padding: '0px'}}> &nbsp; &nbsp; price/High (adjust expected price) &nbsp;
+        <label style={{margin: '0px', padding: '0px'}}> &nbsp; &nbsp; adjust expected price (price/High) &nbsp;
           <input style={{width: '50px'}} type="number" step="0.01" name="priceDivHighFactor" // required="required"
             placeholder={priceDivHighFactor} 
               onChange={(e) => setPriceDivHighFactor (e.target.value)} value={priceDivHighFactor}  />
