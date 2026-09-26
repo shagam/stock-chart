0ae3398b21671e265866005c9b64f147103cce81
shagam <eli.shagam@gmail.com>
2026-09-26 22:03:57 +0300

    Change file according to new version of React 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css"
 require('dotenv').config();
 // console.log (process.env)
-ReactDOM.render(
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
-    <div>
-       <App />
-    </div>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
